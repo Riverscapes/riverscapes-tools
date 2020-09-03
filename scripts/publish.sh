facee9c1bf3c2241df5ad9a2809f60c75471dad2
Matt Reimer <matt.reimer@gmail.com>
2020-09-03 10:28:53 -0700

    script changes

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,22 +9,9 @@ function jekyllbuild () {
 }
 
 jekyllbuild $ORIGPWD/docs
-echo "HERE: $ORIGPWD, $ORIGPWD/_site"
-mv $ORIGPWD/docs/_site $ORIGPWD/
-
 
 for file in $ORIGPWD/packages/*/docs ; do 
   if [[ -d "$file" && ! -L "$file" ]]; then
-    echo "$file is a directory"; 
+    jekyllbuild $file
   fi; 
 done
-
-# # Now for each known site build it and move it
-# for i in "${docfolders[@]}"
-# do
-#    :
-#    SITE=$PWD/packages/$i/docs
-#    jekyllbuild $SITE
-#    mv $SITE/_site $PWD/_site/$i
-#    # do whatever on $i
-# done
