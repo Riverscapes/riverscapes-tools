ce24cdcf7321d5e60d3cbd3a4b87113df7a399fd
Matt Reimer <matt.reimer@gmail.com>
2020-10-19 10:06:03 -0700

    adding RVD as a package

---
+++ packages/rvd/database/rvd_schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Epochs (EpochID INTEGER PRIMARY KEY NOT NULL, Name TEXT NOT NULL UNIQUE, Metadata TEXT, Notes TEXT);
+CREATE TABLE ReachCodes (ReachCode INTEGER PRIMARY KEY NOT NULL, Name TEXT NOT NULL, DisplayName TEXT, Description TEXT NOT NULL);
+CREATE TABLE ReachVegetation (ReachID INTEGER REFERENCES Reaches ON DELETE CASCADE NOT NULL, VegetationID INTEGER REFERENCES VegetationTypes (VegetationID) NOT NULL, Area REAL NOT NULL CONSTRAINT CHK_ReachVegetation_Area CHECK (Area > 0), CellCount REAL NOT NULL CONSTRAINT CHK_ReachVegetation_CellCount CHECK (CellCount > 0), PRIMARY KEY (ReachID, VegetationID));
+CREATE TABLE MetaData (KeyInfo TEXT PRIMARY KEY NOT NULL, ValueInfo TEXT);
+CREATE TABLE Watersheds (WatershedID TEXT PRIMARY KEY NOT NULL UNIQUE, Name TEXT NOT NULL, AreaSqKm REAL CONSTRAINT CHK_HUCs_Area CHECK (AreaSqKm >= 0), States TEXT, Metadata TEXT, Notes TEXT);
+CREATE TABLE VegetationTypes (VegetationID INTEGER PRIMARY KEY NOT NULL, EpochID INTEGER REFERENCES Epochs (EpochID) NOT NULL, Name TEXT NOT NULL, Physiognomy TEXT, Notes TEXT);
+CREATE TABLE Reaches (ReachID INTEGER PRIMARY KEY NOT NULL, WatershedID TEXT REFERENCES Watersheds (WatershedID) ON DELETE CASCADE, Geometry TEXT, ReachCode INTEGER REFERENCES ReachCodes (ReachCode), IsPeren INTEGER NOT NULL DEFAULT (0), StreamName TEXT, Orig_DA REAL, iGeo_DA REAL);
+CREATE INDEX FK_ReachVegetation_ReachID ON ReachVegetation (ReachID);
+CREATE INDEX FK_ReachVegetation_VegetationID ON ReachVegetation (VegetationID);
+CREATE INDEX IX_Watersheds_States ON Watersheds (States);
+CREATE INDEX FK_VegetationTypes_EpochID ON VegetationTypes (EpochID);
+CREATE INDEX FX_Reaches_FCode ON Reaches (ReachCode);
+CREATE INDEX FX_Reaches_HUC ON Reaches (WatershedID);
