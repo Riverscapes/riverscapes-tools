46b171f392920375e3aea608edcde484949db680
Matt Reimer <matt.reimer@gmail.com>
2020-11-10 16:15:04 -0800

    vbet now has a report

--- packages/rscontext/rscontext/rs_context_report.py
+++ packages/rscontext/rscontext/rs_context_report.py
@@ -14,7 +14,7 @@ class RSContextReport(RSReport):
 
     def __init__(self, report_path, rs_project, project_root):
         super().__init__(rs_project, report_path)
-        self.log = Logger('RSContext')
+        self.log = Logger('RSContext Report')
         self.project_root = project_root
         self.report_intro()
 
@@ -22,36 +22,14 @@ class RSContextReport(RSReport):
         section = self.section('LayerSummary', 'Layer Summary')
         layers = self.xml_project.XMLBuilder.find('Realizations').find('RSContext').getchildren()
 
-        [self.layerprint(lyr, section) for lyr in layers if lyr.tag in ['DEM', 'Raster', 'Vector']]
-        [self.layerprint(lyr, section) for lyr in layers if lyr.tag in ['SQLiteDB']]
-
-    def layerprint(self, lyr_el, section):
-        tag = lyr_el.tag
-        name = lyr_el.find('Name').text
-
-        section = self.section(None, '{}: {}'.format(tag, name), level=2, attrib={'class': 'rsc-layer'})
-
-        meta = self.xml_project.get_metadata_dict(node=lyr_el)
-        if meta is not None:
-            self.create_table_from_dict(meta, section, attrib={'class': 'fullwidth'})
-
-        path_el = ET.Element('pre', attrib={'class': 'path'})
-        pathstr = lyr_el.find('Path').text
-        size = 0
-        fpath = os.path.join(self.project_root, pathstr)
-        if os.path.isfile(fpath):
-            size = os.path.getsize(fpath)
-
-        footer = ET.Element('div', attrib={'class': 'layer-footer'})
-        path_el.text = 'Project path: {}  ({})'.format(pathstr, sizeof_fmt(size))
-        footer.append(path_el)
-        section.append(footer)
+        [self.layerprint(lyr, section, self.project_root) for lyr in layers if lyr.tag in ['DEM', 'Raster', 'Vector']]
+        [self.layerprint(lyr, section, self.project_root) for lyr in layers if lyr.tag in ['SQLiteDB']]
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('projectxml', help='Path to the BRAT project.rs.xml', type=str)
+    parser.add_argument('projectxml', help='Path to the project.rs.xml', type=str)
     parser.add_argument('report_path', help='Output path where report will be generated', type=str)
     args = dotenv.parse_args_env(parser)
 
