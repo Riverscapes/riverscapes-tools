92413a575e1efc118b2811d1cb618310768a446d
Philip Bailey <philip@northarrowresearch.com>
2020-12-29 15:58:02 -0800

    Reaches split into two tables. Up to Reach Geometry working

--- packages/brat/sqlbrat/utils/reach_geometry.py
+++ packages/brat/sqlbrat/utils/reach_geometry.py
@@ -22,7 +22,7 @@ from rscommons import Logger, ProgressBar, dotenv, VectorBase
 from rscommons.shapefile import _rough_convert_metres_to_raster_units
 from rscommons.shapefile import get_utm_zone_epsg
 from rscommons.shapefile import get_transform_from_epsg
-from rscommons.database import load_geometries, write_attributes, get_db_srs
+from rscommons.database import load_geometries, write_attributes_NEW, get_db_srs
 from rscommons.raster_buffer_stats import raster_buffer_stats2
 from rscommons.classes.vector_classes import get_shp_or_gpkg
 
@@ -33,13 +33,6 @@ def reach_geometry_NEW(flow_lines: Path, dem_path: Path, buffer_distance: float,
 
     log = Logger('Reach Geometry')
 
-    # TODO: check with Matt the best way to reproject vectors onto raster projection
-    dataset = gdal.Open(dem_path)
-    gt = dataset.GetGeoTransform()
-    db_srs = get_db_srs(database)
-    gdalSRS_wkt = dataset.GetProjection()
-    rasterSRS = osr.SpatialReference(wkt=gdalSRS_wkt)
-
     # Buffer the start and end point of each reach
     line_start_polygons = {}
     line_end_polygons = {}
@@ -47,39 +40,72 @@ def reach_geometry_NEW(flow_lines: Path, dem_path: Path, buffer_distance: float,
     with get_shp_or_gpkg(flow_lines) as lyr:
 
         vector_buffer = lyr.rough_convert_metres_to_vector_units(buffer_distance)
-        transform_to_raster = lyr.get_transform(lyr.spatial_ref, out_srs)
-        transform_to_output = lyr.get_transform_from_epsg(epsg)
+        _srs, transform_to_raster = lyr.get_transform_from_raster(dem_path)
+        _srs, transform_to_output = lyr.get_transform_from_epsg(epsg)
 
         for feature, _counter, _progbar in lyr.iterate_features("Processing reaches"):
-            reach_id = feature.GetField('ReachID')
+            reach_id = feature.GetFID()
             geom = feature.GetGeometryRef()
 
             # Calculate the reach length in the output spatial reference
             if transform_to_output is not None:
                 geom.Transform(transform_to_output)
-            reaches[reach_id] = {'iGeo_Len': geom.Length, 'iGeo_Slope': 0.0, 'iGeo_ElMin': None, 'Igeo_ElMax': None}
+
+            reaches[reach_id] = {'iGeo_Len': geom.Length(), 'iGeo_Slope': 0.0, 'iGeo_ElMin': None, 'IGeo_ElMax': None}
 
             # Buffer the ends of the reach polyline in the raster spatial reference
             if transform_to_raster is not None:
                 geom.Transform(transform_to_raster)
 
-            line_start_polygons[reach_id] = VectorBase.ogr2shapely(geom, transform_to_raster).coords[0].buffer(vector_buffer)
-            line_end_polygons[reach_id] = VectorBase.ogr2shapely(geom, transform_to_raster).coords[0].buffer(vector_buffer)
+            line_start_polygons[reach_id] = Point(VectorBase.ogr2shapely(geom, transform_to_raster).coords[0]).buffer(vector_buffer)
+            line_end_polygons[reach_id] = Point(VectorBase.ogr2shapely(geom, transform_to_raster).coords[-1]).buffer(vector_buffer)
 
-    # Retrieve the mean elevation of each point
+    # Retrieve the mean elevation of start and end of point
     line_start_elevations = raster_buffer_stats2(line_start_polygons, dem_path)
     line_end_elevations = raster_buffer_stats2(line_end_polygons, dem_path)
 
     for reach_id, data in reaches.items():
         if reach_id in line_start_elevations and reach_id in line_end_elevations:
-            data['iGeo_ElMax'] = max(line_start_elevations['Max'], line_end_elevations['Max']),
-            data['iGeo_ElMin'] = min(line_start_elevations['Min'], line_end_elevations['Min'])
-            if line_start_elevations['Mean'] != line_end_elevations['Mean']:
-                data['iGeo_Slope'] = abs(line_start_elevations['Mean'] - line_end_elevations['Mean']) / data['iGeo_Len']
+            sta_data = line_start_elevations[reach_id]
+            end_data = line_end_elevations[reach_id]
+
+            data['iGeo_ElMax'] = _max_ignore_none(sta_data['Maximum'], end_data['Maximum'])
+            data['iGeo_ElMin'] = _min_ignore_none(sta_data['Minimum'], end_data['Minimum'])
+
+            if sta_data['Mean'] is not None and end_data['Mean'] is not None and sta_data['Mean'] != end_data['Mean']:
+                data['iGeo_Slope'] = abs(sta_data['Mean'] - end_data['Mean']) / data['iGeo_Len']
         else:
             log.warning('{:,} features skipped because one or both ends of polyline not on DEM raster'.format(dem_path))
 
-    write_attributes(os.path.dirname(flow_lines), reaches, ['iGeo_Len', 'iGeo_ElMax', 'iGeo_ElMin', 'iGeo_Slope'])
+    write_attributes_NEW(os.path.dirname(flow_lines), reaches, ['iGeo_Len', 'iGeo_ElMax', 'iGeo_ElMin', 'iGeo_Slope'])
+
+
+def _max_ignore_none(val1, val2):
+
+    if val1 is not None:
+        if val2 is not None:
+            return max(val1, val2)
+        else:
+            return val1
+    else:
+        if val2 is not None:
+            return val2
+        else:
+            return None
+
+
+def _min_ignore_none(val1, val2):
+
+    if val1 is not None:
+        if val2 is not None:
+            return min(val1, val2)
+        else:
+            return val1
+    else:
+        if val2 is not None:
+            return val2
+        else:
+            return None
 
 
 # def reach_geometry(database, dem_path, buffer_distance, epsg):
