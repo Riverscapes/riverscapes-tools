6997ddeaea09d367ce7335133511fcfd8f2c67a2
Philip Bailey <philip@northarrowresearch.com>
2020-12-31 08:31:21 -0800

    BRAT run code refinements

--- packages/brat/sqlbrat/brat_run.py
+++ packages/brat/sqlbrat/brat_run.py
@@ -1,13 +1,13 @@
-# Name:   BRAT Run
-#
-#         Run the BRAT model on an existing BRAT project that was built
-#         using the brat_build.py script. The model calculates existing
-#         and historic dam capacities as well as conservation and conflict.
-#
-# Author: Philip Bailey
-#
-# Date:   30 May 2019
-# -------------------------------------------------------------------------------
+""" Run the BRAT model on an existing BRAT project that was built
+    using the brat_build.py script. The model calculates existing
+    and historic dam capacities as well as conservation and conflict.
+
+    Philip Bailey
+    30 May 2019
+
+    Returns:
+        None: None
+"""
 import os
 import sys
 import traceback
@@ -16,7 +16,7 @@ import time
 import datetime
 from osgeo import ogr
 from rscommons import Logger, RSLayer, RSProject, ModelConfig, dotenv
-from rscommons.database import execute_query, update_database, store_metadata, set_reach_fields_null_NEW, SQLiteCon
+from rscommons.database import update_database, store_metadata, set_reach_fields_null_NEW, SQLiteCon
 from sqlbrat.utils.vegetation_suitability import vegetation_suitability, output_vegetation_raster
 from sqlbrat.utils.vegetation_fis import vegetation_fis
 from sqlbrat.utils.combined_fis import combined_fis
@@ -30,14 +30,12 @@ cfg = ModelConfig('http://xml.riverscapes.xyz/Projects/XSD/V1/BRAT.xsd', __versi
 
 # Dictionary of fields that this process outputs, keyed by ShapeFile data type
 output_fields = {
-    ogr.OFTString: ['Risk', 'Limitation', 'Opportunity'],
     ogr.OFTInteger: ['RiskID', 'LimitationID', 'OpportunityID'],
     ogr.OFTReal: ['iVeg100EX', 'iVeg_30EX', 'iVeg100HPE', 'iVeg_30HPE', 'iPC_LU',
                   'iPC_VLowLU', 'iPC_LowLU', 'iPC_ModLU', 'iPC_HighLU', 'iHyd_QLow',
                   'iHyd_Q2', 'iHyd_SPLow', 'iHyd_SP2', 'oVC_HPE', 'oVC_EX', 'oCC_HPE',
                   'mCC_HPE_CT', 'oCC_EX', 'mCC_EX_CT', 'mCC_HisDep']
 }
-
 
 LayerTypes = {
     'EXVEG_SUIT': RSLayer('Existing Vegetation', 'EXVEG_SUIT', 'Raster', 'intermediates/existing_veg_suitability.tif'),
@@ -84,61 +82,61 @@ def brat_run(project_root, csv_dir):
     outputs_node = r_node.find('Outputs')
 
     # Get the filepaths for the DB and shapefile
-    database = os.path.join(project.project_dir, r_node.find('Outputs/Geopackage[@id="OUTPUTS"]/Path').text)
+    gpkg_path = os.path.join(project.project_dir, r_node.find('Outputs/Geopackage[@id="OUTPUTS"]/Path').text)
 
-    if not os.path.isfile(database):
-        raise Exception('BRAT geopackage file missing at {}. You must run Brat Build first.'.format(database))
+    if not os.path.isfile(gpkg_path):
+        raise Exception('BRAT geopackage file missing at {}. You must run Brat Build first.'.format(gpkg_path))
 
     # Update any of the lookup tables we need
-    update_database(database, csv_dir)
+    update_database(gpkg_path, csv_dir)
 
     # Store the BRAT Run date time to the database (for reporting)
-    store_metadata(database, 'BRAT_Run_DateTime', datetime.datetime.now().isoformat())
+    store_metadata(gpkg_path, 'BRAT_Run_DateTime', datetime.datetime.now().isoformat())
 
-    watershed, max_drainage_area, ecoregion = get_watershed_info(database)
+    watershed, max_drainage_area, ecoregion = get_watershed_info(gpkg_path)
 
     # Set database output columns to NULL before processing (note omission of string lookup fields from view)
-    set_reach_fields_null_NEW(database, output_fields[ogr.OFTReal])
-    set_reach_fields_null_NEW(database, output_fields[ogr.OFTInteger])
+    set_reach_fields_null_NEW(gpkg_path, output_fields[ogr.OFTReal])
+    set_reach_fields_null_NEW(gpkg_path, output_fields[ogr.OFTInteger])
 
     # Calculate the low and high flow using regional discharge equations
-    hydrology(database, 'Low', watershed)
-    hydrology(database, '2', watershed)
+    hydrology(gpkg_path, 'Low', watershed)
+    hydrology(gpkg_path, '2', watershed)
 
     # Calculate the vegetation and combined FIS for the existing and historical vegetation epochs
     for epoch, prefix, ltype, orig_id in Epochs:
 
         # Calculate the vegetation suitability for each buffer
-        [vegetation_suitability(database, buffer, prefix, ecoregion) for buffer in get_stream_buffers(database)]
+        [vegetation_suitability(gpkg_path, buffer, prefix, ecoregion) for buffer in get_stream_buffers(gpkg_path)]
 
         # Run the vegetation and then combined FIS for this epoch
-        vegetation_fis(database, epoch, prefix)
-        combined_fis(database, epoch, prefix, max_drainage_area)
+        vegetation_fis(gpkg_path, epoch, prefix)
+        combined_fis(gpkg_path, epoch, prefix, max_drainage_area)
 
         orig_raster = os.path.join(project.project_dir, input_node.find('Raster[@id="{}"]/Path'.format(orig_id)).text)
         _veg_suit_raster_node, veg_suit_raster = project.add_project_raster(intermediate_node, LayerTypes[ltype], None, True)
-        output_vegetation_raster(database, orig_raster, veg_suit_raster, epoch, prefix, ecoregion)
+        output_vegetation_raster(gpkg_path, orig_raster, veg_suit_raster, epoch, prefix, ecoregion)
 
     # Calculate departure from historical conditions
-    with SQLiteCon(database) as db:
+    with SQLiteCon(gpkg_path) as database:
         log.info('Calculating departure from historic conditions')
-        db.curs.execute('UPDATE ReachAttributes SET mCC_HisDep = mCC_HPE_CT - mCC_EX_CT WHERE (mCC_EX_CT IS NOT NULL) AND (mCC_HPE_CT IS NOT NULL)')
-        db.conn.commit()
+        database.curs.execute('UPDATE ReachAttributes SET mCC_HisDep = mCC_HPE_CT - mCC_EX_CT WHERE (mCC_EX_CT IS NOT NULL) AND (mCC_HPE_CT IS NOT NULL)')
+        database.conn.commit()
 
     # Land use intesity, conservation and restoration
-    land_use(database, 100.0)
-    conservation(database)
+    land_use(gpkg_path, 100.0)
+    conservation(gpkg_path)
 
     report_path = os.path.join(project.project_dir, LayerTypes['BRAT_RUN_REPORT'].rel_path)
     project.add_report(outputs_node, LayerTypes['BRAT_RUN_REPORT'], replace=True)
 
-    report = BratReport(database, report_path, project)
+    report = BratReport(gpkg_path, report_path, project)
     report.write()
 
     log.info('BRAT run complete')
 
 
-def get_watershed_info(database):
+def get_watershed_info(gpkg_path):
     """Query a BRAT database and get information about
     the watershed being run. Assumes that all watersheds
     except the one being run have been deleted.
@@ -151,9 +149,9 @@ def get_watershed_info(database):
         the watershed is associated.
     """
 
-    with SQLiteCon(database) as db:
-        db.curs.execute('SELECT WatershedID, MaxDrainage, EcoregionID FROM Watersheds')
-        row = db.curs.fetchone()
+    with SQLiteCon(gpkg_path) as database:
+        database.curs.execute('SELECT WatershedID, MaxDrainage, EcoregionID FROM Watersheds')
+        row = database.curs.fetchone()
         watershed = row['WatershedID']
         max_drainage = row['MaxDrainage']
         ecoregion = row['EcoregionID']
@@ -172,7 +170,7 @@ def get_watershed_info(database):
     return watershed, max_drainage, ecoregion
 
 
-def get_stream_buffers(database):
+def get_stream_buffers(gpkg_path):
     """Get the list of buffers used to sample the vegetation.
     Assumes that the vegetation has already been sample and that
     the streamside and riparian buffers are the only values in
@@ -185,12 +183,14 @@ def get_stream_buffers(database):
         [list] -- all discrete vegetation buffers
     """
 
-    with SQLiteCon(database) as db:
-        db.curs.execute('SELECT Buffer FROM ReachVegetation GROUP BY Buffer')
-        return [row['Buffer'] for row in db.curs.fetchall()]
+    with SQLiteCon(gpkg_path) as database:
+        database.curs.execute('SELECT Buffer FROM ReachVegetation GROUP BY Buffer')
+        return [row['Buffer'] for row in database.curs.fetchall()]
 
 
 def main():
+    """ Main BRAT Run
+    """
     parser = argparse.ArgumentParser(
         description='Run brat against a pre-existing sqlite db:',
         # epilog="This is an epilog"
