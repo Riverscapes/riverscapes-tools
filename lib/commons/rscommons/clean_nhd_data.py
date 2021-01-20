d5ed6ef6e596aa7df2cf00e7f46f3cf20507fedc
Matt Reimer <matt.reimer@gmail.com>
2021-01-19 16:07:40 -0800

    Reworking the approach #154 #156

--- lib/commons/rscommons/clean_nhd_data.py
+++ lib/commons/rscommons/clean_nhd_data.py
@@ -4,7 +4,7 @@ from osgeo import ogr
 from rscommons.science_base import get_nhdhr_url
 from rscommons.download import download_unzip
 from rscommons.filegdb import export_feature_class, copy_attributes, export_table
-from rscommons.util import safe_makedirs, safe_remove_dir
+from rscommons.util import safe_makedirs
 from rscommons.shapefile import get_geometry_union
 
 # NHDPlus HR Value Added Attributes (VAA) to be copied to the NHDFlowline feature class
