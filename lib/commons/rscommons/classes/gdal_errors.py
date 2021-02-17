d18e37dbcf8890f3fa428f32d8273ed41d7382d1
Matt <matt.reimer@gmail.com>
2021-02-17 09:20:27 -0800

    Better errors addresses #223

--- lib/commons/rscommons/classes/gdal_errors.py
+++ lib/commons/rscommons/classes/gdal_errors.py
@@ -1,6 +1,7 @@
+import os
 from osgeo import ogr
 from osgeo import gdal
-import osgeo
+from rscommons.classes.logger import Logger
 
 CREATED = False
 
@@ -18,9 +19,20 @@ class _GdalErrorHandler(object):
         self.err_msg = ''
 
     def handler(self, err_level, err_no, err_msg):
+        logh = Logger('GDALError')
         self.err_level = err_level
         self.err_no = err_no
         self.err_msg = err_msg
+        err_str = "ERR# {} -- {}".format(self.err_no, self.err_msg)
+
+        if self.err_level in [gdal.CE_Failure, gdal.CE_Fatal]:
+            logh.error(err_str)
+        elif self.err_level == gdal.CE_Warning:
+            logh.warning(err_str)
+        elif self.err_level == gdal.CE_Debug:
+            logh.debug(err_str)
+        else:
+            logh.info(err_str)
 
 
 def initGDALOGRErrors():
@@ -35,18 +47,22 @@ def initGDALOGRErrors():
         # due to a reference counting bug
         # http://trac.osgeo.org/gdal/ticket/5186#comment:4
 
-        gdal.PushErrorHandler(handler)
         gdal.UseExceptions()  # Exceptions will get raised on anything >= gdal.CE_Failure
         ogr.UseExceptions()
+        gdal.PushErrorHandler(handler)
         CREATED = True
 
 
 if __name__ == '__main__':
+    log = Logger('Test GDAL Errors')
+    log.setup(logPath=os.path.join("./GDAL_Error.log"), verbose=True)
 
     initGDALOGRErrors()
 
     try:
-        gdal.Error(gdal.CE_Warning, 1, 'Test warning message')
+        gdal.Error(gdal.CE_Debug, 1222, 'Test debug message')
+        gdal.Error(gdal.CE_Warning, 1542, 'Test warning message')
+        gdal.Error(gdal.CE_Failure, 11231, 'Test fatal message')
     except Exception as e:
         print('Operation raised an exception')
         raise e
@@ -55,4 +71,5 @@ if __name__ == '__main__':
     #         print('Operation raised an warning')
     #         raise RuntimeError(err.err_level, err.err_no, err.err_msg)
     finally:
+
         gdal.PopErrorHandler()
