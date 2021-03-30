6d8a0eec44eb29ccca9e945c51da9fbf2726338c
K Whitehead <kellymwhitehead@gmail.com>
2021-03-30 14:11:42 -0700

    drainage area zonal transforms for slope and hand

--- packages/vbet/database/data/inputs.sql
+++ packages/vbet/database/data/inputs.sql
@@ -2,3 +2,7 @@ INSERT INTO inputs (input_id, name, description) VALUES (1, 'Slope', 'slope (deg
 INSERT INTO inputs (input_id, name, description) VALUES (2, 'HAND', 'Height above nearest drainage (metres)');
 INSERT INTO inputs (input_id, name, description) VALUES (3, 'Channel', 'Distance from flow network (metres)');
 INSERT INTO inputs (input_id, name, description) VALUES (4, 'Flow Areas', 'Distance from flow areas (metres)');
+INSERT INTO inputs (input_id, name, description) VALUES (5, 'Slope MID', 'slope (degrees)');
+INSERT INTO inputs (input_id, name, description) VALUES (6, 'HAND MID', 'Height above nearest drainage (metres)');
+INSERT INTO inputs (input_id, name, description) VALUES (7, 'Slope LARGE', 'slope (degrees)');
+INSERT INTO inputs (input_id, name, description) VALUES (8, 'HAND LARGE', 'Height above nearest drainage (metres)');
