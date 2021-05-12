0ffa3de7f450684dce30833d9bbe29cde1a6aa82
Matt <matt.reimer@gmail.com>
2021-05-12 16:47:53 -0700

    fixing the duration util

---
+++ lib/commons/test/test_util.py
@@ -0,0 +1,32 @@
+""" Testing for the vector ops
+
+"""
+import unittest
+from rscommons import util
+
+
+class UtilTest(unittest.TestCase):
+
+    def test_pretty_duration(self):
+
+        test1 = util.pretty_duration(0)
+        self.assertEqual(test1, '0.0 seconds')
+
+        test1 = util.pretty_duration(10)
+        self.assertEqual(test1, '10.0 seconds')
+
+        test1 = util.pretty_duration(100)
+        self.assertEqual(test1, '1:40 minutes')
+
+        test1 = util.pretty_duration(1000)
+        self.assertEqual(test1, '16:40 minutes')
+
+        test1 = util.pretty_duration(10000)
+        self.assertEqual(test1, '2:46 hours')
+
+        test1 = util.pretty_duration(100000)
+        self.assertEqual(test1, '1 days, 3:46 hours')
+
+
+if __name__ == '__main__':
+    unittest.main()
