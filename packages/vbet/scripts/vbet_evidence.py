3c7fb9c4736d450e225c2a4b17408fafcf736ded
K Whitehead <kellymwhitehead@gmail.com>
2021-07-09 16:57:03 -0700

    update fields for vbet_evidence script

--- packages/vbet/scripts/vbet_evidence.py
+++ packages/vbet/scripts/vbet_evidence.py
@@ -12,6 +12,12 @@ attributes = {'HAND': 'HAND.tif',
               'TWI': 'twi.tif',
               'Slope': 'slope.tif'}
 
+observation_fields = {'HUC8': ogr.OFTInteger,
+                      'observationid': ogr.OFTInteger,
+                      'categoryid': ogr.OFTInteger,
+                      'confidence': ogr.OFTReal,
+                      'notes': ogr.OFTString}
+
 
 def extract_vbet_evidence(observation_points, vbet_data_root, out_points):
 
@@ -21,21 +27,27 @@ def extract_vbet_evidence(observation_points, vbet_data_root, out_points):
         out_layer.create_layer_from_ref(in_points)
         for field_name in attributes:
             out_layer.create_field(field_name, ogr.OFTReal)
+        # for field, field_type in observation_fields.items():
+            # out_layer.create_field(field, field_type=field_type)
         out_layer_defn = out_layer.ogr_layer.GetLayerDefn()
 
         for feat, *_ in in_points.iterate_features():
 
-            huc = feat.GetField('HUC8')
+            feat_attributes = {name: feat.GetField(name) for name in observation_fields}
+
             geom = feat.GetGeometryRef()
             new_feat = ogr.Feature(out_layer_defn)
 
             for attribute, path in attributes.items():
-                raster_path = os.path.join(vbet_data_root, huc, 'inputs' if attribute == 'slope' else 'intermediates', path)
+                raster_path = os.path.join(vbet_data_root, feat_attributes['HUC8'], 'inputs' if attribute == 'Slope' else 'intermediates', path)
                 if os.path.exists(raster_path):
                     value = extract_raster_by_point(geom, raster_path)
                 else:
                     value = None
                 new_feat.SetField(attribute, value)
+
+            for field, value in feat_attributes.items():
+                new_feat.SetField(field, value)
 
             new_feat.SetGeometry(geom)
             out_layer.ogr_layer.CreateFeature(new_feat)
