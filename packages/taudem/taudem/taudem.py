4f944eef11241efe518b87d200d81d8fe7bbc54c
K Whitehead <kellymwhitehead@gmail.com>
2021-07-20 16:38:53 -0700

    TauDEM methods updated

--- packages/taudem/taudem/taudem.py
+++ packages/taudem/taudem/taudem.py
@@ -19,6 +19,8 @@ from typing import List, Dict
 
 # LEave OSGEO import alone. It is necessary even if it looks unused
 from osgeo import gdal
+from osgeo.ogr import Layer
+from rscommons.classes.vector_classes import get_shp_or_gpkg, VectorBase
 
 from rscommons.util import safe_makedirs, parse_metadata, safe_remove_dir
 from rscommons import RSProject, RSLayer, ModelConfig, Logger, dotenv, initGDALOGRErrors
@@ -35,51 +37,47 @@ initGDALOGRErrors()
 
 cfg = ModelConfig('http://xml.riverscapes.xyz/Projects/XSD/V1/TauDEM.xsd', __version__)
 
+NCORES = os.environ['TAUDEM_CORES'] if 'TAUDEM_CORES' in os.environ else '2'
+
 LayerTypes = {
     'DEM': RSLayer('DEM', 'DEM', 'Raster', 'inputs/dem.tif'),
     'HILLSHADE': RSLayer('DEM Hillshade', 'HILLSHADE', 'Raster', 'inputs/dem_hillshade.tif'),
     'INPUTS': RSLayer('Inputs', 'INPUTS', 'Geopackage', 'inputs/hand_inputs.gpkg', {
-        'FLOWLINES': RSLayer('NHD Flowlines', 'FLOWLINES', 'Vector', 'flowlines'),
+        'CHANNEL_AREA': RSLayer('Channel Area Polygons', 'CHANNEL_AREA', 'Vector', 'channel_areas'),
+        'CHANNEL_LINES': RSLayer('Channel Lines', 'CHANNEL_LINES', 'Vector', 'channel_lines'),
         'DEM_MASK_POLY': RSLayer('DEM Mask Polygon', 'DEM_MASK_POLY', 'Vector', 'dem_mask_poly'),
-        'FLOW_AREA': RSLayer('NHD Flow Areas', 'FLOW_AREA', 'Vector', 'flow_areas'),
-        'HAND_NETWORK': RSLayer('HAND Network', 'HAND_NETWORK', 'Vector', 'hand_network'),
     }),
 
     # Intermediate Products
     'DEM_MASKED': RSLayer('DEM Masked', 'DEM_MASKED', 'Raster', 'intermediates/dem_masked.tif'),
-    'PITFILL': RSLayer('TauDEM Pitfill', 'PITFILL', 'Raster', 'intermediates/hand_processing/pitfill.tif'),
-    'DINFFLOWDIR_SLP': RSLayer('TauDEM D-Inf Flow Directions Slope', 'DINFFLOWDIR_SLP', 'Raster', 'intermediates/hand_processing/dinfflowdir_slp.tif'),
-    'DINFFLOWDIR_ANG': RSLayer('TauDEM D-Inf Flow Directions', 'DINFFLOWDIR_ANG', 'Raster', 'intermediates/hand_processing/dinfflowdir_ang.tif'),
-    'RASTERIZED_FLOWLINES': RSLayer('Rasterized Flowlines', 'RASTERIZED_FLOWLINES', 'Raster', 'intermediates/rasterized_flowline.tif'),
-    'RASTERIZED_FLOWAREAS': RSLayer('Rasterized Flowareas', 'RASTERIZED_FLOWAREAS', 'Raster', 'intermediates/rasterized_flowareas.tif'),
-    'RASTERIZED_DRAINAGE': RSLayer('Rasterized Drainage', 'RASTERIZED_DRAINAGE', 'Raster', 'intermediates/rasterized_drainage.tif'),
-
-    'INTERMEDIATES': RSLayer('Intermediates', 'INTERMEIDATES', 'Geopackage', 'intermediates/hand_intermediates.gpkg', {
-        'BUFFERED_FLOWLINES': RSLayer('Buffered Flowlines', 'BUFFERED_FLOWLINES', 'Vector', 'buffered_flowlines'),
-        'DRAINAGE_POLYGONS': RSLayer('Drainage Polygons', 'DRAINAGE_POLYGONS', 'Vector', 'drainage_polygons'),
-    }),
+    'PITFILL': RSLayer('TauDEM Pitfill', 'PITFILL', 'Raster', 'intermediates/pitfill.tif'),
+    'DINFFLOWDIR_ANG': RSLayer('TauDEM D-Inf Flow Directions', 'DINFFLOWDIR_ANG', 'Raster', 'intermediates/dinfflowdir_ang.tif'),
+    'AREADINF_SCA': RSLayer('TauDEM D-Inf Contributing Area', 'AREADINF_SCA', 'Raster', 'intermediates/areadinf_sca.tif'),
+    'RASTERIZED_CHANNEL': RSLayer('Rasterized Channel', 'RASTERIZED_CHANNEL', 'Raster', 'intermediates/rasterized_channel.tif'),
+    # 'INTERMEDIATES': RSLayer('Intermediates', 'INTERMEIDATES', 'Geopackage', 'intermediates/hand_intermediates.gpkg', {
+    # }),
 
     # Outputs:
+    'DINFFLOWDIR_SLP': RSLayer('TauDEM D-Inf Flow Directions Slope', 'DINFFLOWDIR_SLP', 'Raster', 'outputs/dinfflowdir_slp.tif'),
     'HAND_RASTER': RSLayer('Hand Raster', 'HAND_RASTER', 'Raster', 'outputs/HAND.tif'),
     'TWI_RASTER': RSLayer('TWI Raster', 'TWI_RASTER', 'Raster', 'outputs/twi.tif'),
     'REPORT': RSLayer('RSContext Report', 'REPORT', 'HTMLFile', 'outputs/hand.html')
 }
 
 
-def taudem(huc, flowlines_orig, orig_dem, hillshade, project_folder, flowareas_orig=None, mask_lyr_path: str = None, keep_intermediates: bool = True, reach_codes: List[str] = None, meta: Dict[str, str] = None, buffer_field: str = None):
+def taudem(huc, input_channel_vector, orig_dem, hillshade, project_folder, mask_lyr_path: str = None, meta: Dict[str, str] = None):
     """[summary]
 
     Args:
-        huc ([str]): [description]
-        flowlines_orig ([str]): [description]
-        flowareas_orig ([str]): [description]
-        orig_dem ([str]): [description]
-        hillshade ([str]): [description]
-        project_folder ([str]): [description]
-        mask_lyr_path ([str]): [description]
-        reach_codes (List[int]): NHD reach codes for features to include in outputs
-        meta (Dict[str,str]): dictionary of riverscapes metadata key: value pairs
+        huc ([type]): [description]
+        input_channel_vector ([type]): [description]
+        orig_dem ([type]): [description]
+        hillshade ([type]): [description]
+        project_folder ([type]): [description]
+        mask_lyr_path (str, optional): [description]. Defaults to None.
+        meta (Dict[str, str], optional): [description]. Defaults to None.
     """
+
     log = Logger('TauDEM')
     log.info('Starting TauDEM v.{}'.format(cfg.version))
 
@@ -96,43 +94,28 @@ def taudem(huc, flowlines_orig, orig_dem, hillshade, project_folder, flowareas_o
 
     # Copy input shapes to a geopackage
     inputs_gpkg_path = os.path.join(project_folder, LayerTypes['INPUTS'].rel_path)
-    intermeidates_gpkg_path = os.path.join(project_folder, LayerTypes['INTERMEDIATES'].rel_path)
+    # intermeidates_gpkg_path = os.path.join(project_folder, LayerTypes['INTERMEDIATES'].rel_path)
+    intermediates_path = os.path.join(project_folder, 'intermediates')
 
-    flowlines_path = os.path.join(inputs_gpkg_path, LayerTypes['INPUTS'].sub_layers['FLOWLINES'].rel_path)
-    flowareas_path = os.path.join(inputs_gpkg_path, LayerTypes['INPUTS'].sub_layers['FLOW_AREA'].rel_path) if flowareas_orig else None
+    with get_shp_or_gpkg(input_channel_vector) as in_layer:
+        channel_vector_type = in_layer.ogr_geom_type
+
+    channel_vector = os.path.join(inputs_gpkg_path, LayerTypes['INPUTS'].sub_layers['CHANNEL_LINES'].rel_path) if channel_vector_type in VectorBase.LINE_TYPES else os.path.join(inputs_gpkg_path, LayerTypes['INPUTS'].sub_layers['CHANNEL_AREA'].rel_path)
     dem_mask_path = os.path.join(inputs_gpkg_path, LayerTypes['INPUTS'].sub_layers['DEM_MASK_POLY'].rel_path) if mask_lyr_path else None
 
     # Make sure we're starting with a fresh slate of new geopackages
     GeopackageLayer.delete(inputs_gpkg_path)
-    GeopackageLayer.delete(intermeidates_gpkg_path)
+    # GeopackageLayer.delete(intermeidates_gpkg_path)
 
-    copy_feature_class(flowlines_orig, flowlines_path, epsg=cfg.OUTPUT_EPSG)
+    copy_feature_class(input_channel_vector, channel_vector, epsg=cfg.OUTPUT_EPSG)
 
     if mask_lyr_path is not None:
         copy_feature_class(mask_lyr_path, dem_mask_path, epsg=cfg.OUTPUT_EPSG)
 
-    if flowareas_orig is not None:
-        copy_feature_class(flowareas_orig, flowareas_path, epsg=cfg.OUTPUT_EPSG)
-
     project.add_project_geopackage(proj_nodes['Inputs'], LayerTypes['INPUTS'])
-
-    # Create a copy of the flow lines with just the perennial and also connectors inside flow areas
-    network_path = os.path.join(inputs_gpkg_path, LayerTypes['INPUTS'].sub_layers['HAND_NETWORK'].rel_path)
-    if reach_codes is not None:
-        vbet_network(flowlines_path, flowareas_path, network_path, cfg.OUTPUT_EPSG, reach_codes)
-    else:
-        copy_feature_class(flowlines_path, network_path, epsg=cfg.OUTPUT_EPSG)
     ##########################################################################
     # The main event:ce
     ##########################################################################
-
-    # Generate HAND from dem and vbet_network
-    # TODO make a place for this temporary folder. it can be removed after hand is generated.
-    temp_hand_dir = os.path.join(project_folder, "intermediates", "hand_processing")
-    safe_makedirs(temp_hand_dir)
-
-    hand_raster = os.path.join(project_folder, LayerTypes['HAND_RASTER'].rel_path)
-    twi_raster = os.path.join(project_folder, LayerTypes['TWI_RASTER'].rel_path)
 
     # If there's no mask we use the original DEM as-is
     hand_dem = proj_dem
@@ -143,40 +126,57 @@ def taudem(huc, flowlines_orig, orig_dem, hillshade, project_folder, flowareas_o
         raster_warp(proj_dem, new_proj_dem, epsg=cfg.OUTPUT_EPSG, clip=dem_mask_path, raster_compression=" -co COMPRESS=LZW -co PREDICTOR=3")
         hand_dem = new_proj_dem
 
-    if buffer_field is not None:
-        buffered_flowlines = os.path.join(intermeidates_gpkg_path, LayerTypes['INTERMEDIATES'].sub_layers['BUFFERED_FLOWLINES'].rel_path)
-        buffer_by_field(network_path, buffered_flowlines, buffer_field, epsg=cfg.OUTPUT_EPSG, centered=True)
-    else:
-        buffered_flowlines = network_path
+    path_rasterized_drainage = os.path.join(project_folder, LayerTypes['RASTERIZED_CHANNEL'].rel_path)
+    hand_rasterize(channel_vector, hand_dem, path_rasterized_drainage)
+    project.add_project_raster(proj_nodes['Intermediates'], LayerTypes['RASTERIZED_CHANNEL'])
 
-    path_rasterized_flowline = os.path.join(project_folder, LayerTypes['RASTERIZED_FLOWLINES'].rel_path)
-    hand_rasterize(buffered_flowlines, hand_dem, path_rasterized_flowline)
+    start_time = time.time()
+    log.info('Starting TauDEM processes')
 
-    if flowareas_path:
-        path_rasterized_flowarea = os.path.join(project_folder, LayerTypes['RASTERIZED_FLOWAREAS'].rel_path)
-        hand_rasterize(flowareas_path, hand_dem, path_rasterized_flowarea)
+    # PitRemove
+    log.info("Filling DEM pits")
+    path_pitfill = os.path.join(project_folder, LayerTypes['PITFILL'].rel_path)
+    pitfill_status = run_subprocess(intermediates_path, ["mpiexec", "-n", NCORES, "pitremove", "-z", hand_dem, "-fel", path_pitfill])
+    if pitfill_status != 0 or not os.path.isfile(path_pitfill):
+        raise Exception('TauDEM: pitfill failed')
+    project.add_project_raster(proj_nodes['Intermediates'], LayerTypes['PITFILL'])
 
-        drainage_raster = os.path.join(project_folder, LayerTypes['RASTERIZED_DRAINAGE'].rel_path)
-        scrips_dir = next(p for p in sys.path if p.endswith('.venv'))
-        script = os.path.join(scrips_dir, 'Scripts', 'gdal_calc.py')
-        run_subprocess(os.path.join(project_folder, "Intermediates"), ['python', script, '-A', path_rasterized_flowline, '-B', path_rasterized_flowarea, f'--outfile={drainage_raster}', '--calc=(A+B)>0', '--co=COMPRESS=LZW'])
-    else:
-        drainage_raster = path_rasterized_flowline
+    # Flow Dir
+    log.info("Finding flow direction")
+    path_ang = os.path.join(project_folder, LayerTypes['DINFFLOWDIR_ANG'].rel_path)
+    path_slp = os.path.join(project_folder, LayerTypes['DINFFLOWDIR_SLP'].rel_path)
+    dinfflowdir_status = run_subprocess(intermediates_path, ["mpiexec", "-n", NCORES, "dinfflowdir", "-fel", path_pitfill, "-ang", path_ang, "-slp", path_slp])
+    if dinfflowdir_status != 0 or not os.path.isfile(path_ang):
+        raise Exception('TauDEM: dinfflowdir failed')
+    project.add_project_raster(proj_nodes['Intermediates'], LayerTypes['DINFFLOWDIR_ANG'])
+    project.add_project_raster(proj_nodes['Outputs'], LayerTypes['DINFFLOWDIR_SLP'])
 
-    create_hand_raster(hand_dem, drainage_raster, temp_hand_dir, hand_raster, out_twi=twi_raster)
-
-    if keep_intermediates is True:
-        project.add_project_raster(proj_nodes['Intermediates'], LayerTypes['DEM_MASKED'])
-        project.add_project_raster(proj_nodes['Intermediates'], LayerTypes['PITFILL'])
-        project.add_project_raster(proj_nodes['Intermediates'], LayerTypes['DINFFLOWDIR_SLP'])
-        project.add_project_raster(proj_nodes['Intermediates'], LayerTypes['DINFFLOWDIR_ANG'])
-        project.add_project_raster(proj_nodes['Intermediates'], LayerTypes['RASTERIZED_FLOWLINES'])
-        project.add_project_geopackage(proj_nodes['Intermediates'], LayerTypes['INTERMEDIATES'])
-    else:
-        safe_remove_dir(temp_hand_dir)
-
+    # generate hand
+    log.info("Generating HAND")
+    hand_raster = os.path.join(project_folder, LayerTypes['HAND_RASTER'].rel_path)
+    dinfdistdown_status = run_subprocess(intermediates_path, ["mpiexec", "-n", NCORES, "dinfdistdown", "-ang", path_ang, "-fel", path_pitfill, "-src", path_rasterized_drainage, "-dd", hand_raster, "-m", "ave", "v"])
+    if dinfdistdown_status != 0 or not os.path.isfile(hand_raster):
+        raise Exception('TauDEM: dinfdistdown failed')
     project.add_project_raster(proj_nodes['Outputs'], LayerTypes['HAND_RASTER'])
+
+    # Generate Flow area
+    log.info("Finding flow area")
+    path_sca = os.path.join(project_folder, LayerTypes['AREADINF_SCA'].rel_path)
+    dinfflowarea_status = run_subprocess(intermediates_path, ["mpiexec", "-n", NCORES, "areadinf", "-ang", path_ang, "-sca", path_sca])
+    if dinfflowarea_status != 0 or not os.path.isfile(path_sca):
+        raise Exception('TauDEM: AreaDinf failed')
+    project.add_project_raster(proj_nodes['Intermediates'], LayerTypes['AREADINF_SCA'])
+
+    # Generate TWI
+    log.info("Generating Topographic Wetness Index (TWI)")
+    twi_raster = os.path.join(project_folder, LayerTypes['TWI_RASTER'].rel_path)
+    twi_status = run_subprocess(intermediates_path, ["mpiexec", "-n", NCORES, "twi", "-slp", path_slp, "-sca", path_sca, '-twi', twi_raster])
+    if twi_status != 0 or not os.path.isfile(twi_raster):
+        raise Exception('TauDEM: TWI failed')
     project.add_project_raster(proj_nodes['Outputs'], LayerTypes['TWI_RASTER'])
+
+    ellapsed_time = time.time() - start_time
+    log.info("TauDEM process complete in {}".format(ellapsed_time))
 
     report_path = os.path.join(project.project_dir, LayerTypes['REPORT'].rel_path)
     project.add_report(proj_nodes['Outputs'], LayerTypes['REPORT'], replace=True)
@@ -195,8 +195,12 @@ def create_project(huc, output_dir):
     project.add_metadata({
         'HUC{}'.format(len(huc)): str(huc),
         'HUC': str(huc),
-        'TauDEMVersion': cfg.version,
-        'TauDEMTimestamp': str(int(time.time()))
+        'TauDEMProjectVersion': cfg.version,
+        'TauDEMTimestamp': str(int(time.time())),
+        'TauDEM_SoftwareVerseion': '5.3.7',
+        'TauDEM_Credits': 'Copyright (C) 2010-2015 David Tarboton, Utah State University',
+        'TauDEM_Licence': 'https://hydrology.usu.edu/taudem/taudem5/GPLv3license.txt',
+        'TauDEM_URL': 'https://hydrology.usu.edu/taudem/taudem5/index.html'
     })
 
     realizations = project.XMLBuilder.add_sub_element(project.XMLBuilder.root, 'Realizations')
@@ -231,18 +235,14 @@ def main():
         # epilog="This is an epilog"
     )
     parser.add_argument('huc', help='NHD flow line ShapeFile path', type=str)
-    parser.add_argument('flowlines', help='NHD flow line ShapeFile path', type=str)
+    parser.add_argument('channel', help='Channel line or area gpkg layer or ShapeFile path', type=str)
     parser.add_argument('dem', help='DEM raster path', type=str)
     parser.add_argument('output_dir', help='Folder where output TauDEM project will be created', type=str)
 
     parser.add_argument('--hillshade', help='Hillshade raster path', type=str)
     parser.add_argument('--mask', help='Optional shapefile to mask by', type=str, default=None)
-    parser.add_argument('--flowareas', help='NHD flow areas ShapeFile path', type=str, default=None)
-    parser.add_argument('--reach_codes', help='Comma delimited reach codes (FCode) to retain when filtering features. Omitting this option retains all features.', type=str)
-    parser.add_argument('--buffer_field', help='buffer field.', type=str)
     parser.add_argument('--meta', help='riverscapes project metadata as comma separated key=value pairs', type=str)
     parser.add_argument('--verbose', help='(optional) a little extra logging ', action='store_true', default=False)
-    parser.add_argument('--intermediates', help='(optional) keep the intermediate products', action='store_true', default=False)
     parser.add_argument('--debug', help='Add debug tools for tracing things like memory usage at a performance cost.', action='store_true', default=False)
 
     args = dotenv.parse_args_env(parser)
@@ -257,17 +257,15 @@ def main():
 
     meta = parse_metadata(args.meta)
 
-    reach_codes = args.reach_codes.split(',') if args.reach_codes else None
-
     try:
         if args.debug is True:
             from rscommons.debug import ThreadRun
             memfile = os.path.join(args.output_dir, 'taudem_mem.log')
-            retcode, max_obj = ThreadRun(taudem, memfile, args.huc, args.flowlines, args.dem, args.hillshade, args.output_dir, args.flowareas, args.mask, args.intermediates, reach_codes, meta)
+            retcode, max_obj = ThreadRun(taudem, memfile, args.huc, args.channel, args.dem, args.hillshade, args.output_dir, args.mask, meta)
             log.debug('Return code: {}, [Max process usage] {}'.format(retcode, max_obj))
 
         else:
-            taudem(args.huc, args.flowlines, args.dem, args.hillshade, args.output_dir, args.flowareas, args.mask, args.intermediates, reach_codes, meta, buffer_field=args.buffer_field)
+            taudem(args.huc, args.channel, args.dem, args.hillshade, args.output_dir, args.mask, meta)
 
     except Exception as e:
         log.error(e)
