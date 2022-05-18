b7930b5141e0eb86dd76c0b3cf459a6167248e96
Philip <philip@northarrowresearch.com>
2022-05-18 12:35:02 -0700

    changes to get RST running on Philips M1 Mac

---
+++ scripts/bootstrap-macm1.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+set -eu
+
+# On OSX you must have run `brew install gdal` so that the header files are findable 
+python3 --version
+python3 -m venv .venv
+# Make sure pip is at a good version
+.venv/bin/python -m pip install --upgrade pip
+.venv/bin/pip --timeout=120 install \
+  Cython==0.29.23
+  # numpy==1.21.1 \
+  # scipy==1.5.1
+  
+.venv/bin/pip install --pre -i https://pypi.anaconda.org/scipy-wheels-nightly/simple scipy
+
+# Need numpy before GDAL
+.venv/bin/pip install GDAL==$(gdal-config --version)
+
+# Now install everything else
+.venv/bin/pip --timeout=120 install -r requirements.txt
+
+# Install our packages as being editable
+.venv/bin/pip install -e ./lib/commons
+.venv/bin/pip install -e ./packages/rscontext
+.venv/bin/pip install -e ./packages/vbet
+.venv/bin/pip install -e ./packages/brat
+.venv/bin/pip install -e ./packages/gnat
+.venv/bin/pip install -e ./packages/hand
+.venv/bin/pip install -e ./packages/rvd
