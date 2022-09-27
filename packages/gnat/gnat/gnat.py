499e47c8bd99dacc0a8d4541b3f7a776a42efcd2
Kelly <kellymwhitehead@gmail.com>
2022-09-27 13:40:37 -0700

    gnat: update metric rounding to 4 decimal places

--- packages/gnat/gnat/gnat.py
+++ packages/gnat/gnat/gnat.py
@@ -581,7 +581,7 @@ def sql_name(name: str) -> str:
 
 def sql_round(datatype: str, metric_id) -> str:
     """return round function"""
-    return f"{'ROUND(' if datatype == 'REAL' else ''}SUM(M.metric_value) FILTER (WHERE M.metric_id == {metric_id}){', 2)' if datatype == 'REAL' else ''}"
+    return f"{'ROUND(' if datatype == 'REAL' else ''}SUM(M.metric_value) FILTER (WHERE M.metric_id == {metric_id}){', 4)' if datatype == 'REAL' else ''}"
 
 
 def generate_metric_list(database: Path, source_table: str = 'metrics') -> dict:
