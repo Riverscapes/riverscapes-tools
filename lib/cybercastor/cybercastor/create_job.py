c59b755c43fe6dc60cf8aa503328c6efa2b5b3a8
Kelly <kellymwhitehead@gmail.com>
2023-05-24 16:34:24 -0700

    org typo

--- lib/cybercastor/cybercastor/create_job.py
+++ lib/cybercastor/cybercastor/create_job.py
@@ -10,7 +10,7 @@ job_types = enum.Enum('rs_context', 'vbet')
 
 
 # create a job file
-def create_job_file(job_name, job_type, hucs, tags, orig_id, visibility='PUBLIC', server="PRODUCTION", description=None, meta=None):
+def create_job_file(job_name, job_type, hucs, tags, org_id, visibility='PUBLIC', server="PRODUCTION", description=None, meta=None):
     """_summary_
 
     Args:
@@ -35,7 +35,7 @@ def create_job_file(job_name, job_type, hucs, tags, orig_id, visibility='PUBLIC'
 
     env = {"TAGS": tags,
            "VISIBILITY": visibility,
-           "ORIG_ID": orig_id}
+           "ORG_ID": org_id}
 
     job_json = {"$schema": "../job.schema.json",
                 "name": job_name,
@@ -56,7 +56,7 @@ if __name__ == "__main__":
     parser.add_argument('job_type', type=str, help='type of job')
     parser.add_argument('hucs', type=str, help='hucs to run the job on')
     parser.add_argument('tags', type=str, help='tags for the job')
-    parser.add_argument('orig_id', type=str, help='original id for the job')
+    parser.add_argument('org_id', type=str, help='orginization id for the job')
     parser.add_argument('--description', type=str, help='description of the job')
     parser.add_argument('--visibility', type=str, help='visibility of the job', default='PUBLIC')
     parser.add_argument('--server', type=str, help='server to run the job on', default='PRODUCTION')
@@ -67,5 +67,5 @@ if __name__ == "__main__":
     meta = {"PROCESSING_GROUP": "1005-1008", "INITIATIVE": "NRCS,CEAP"}
     description = "VBET run for production using all huc10s in 1003 and 1004"
 
-    create_job_file(args.job_name, args.job_type, args.hucs, args.tags, args.orig_id, args.visibility, args.server, args.description, args.meta)
+    create_job_file(args.job_name, args.job_type, args.hucs, args.tags, args.org_id, args.visibility, args.server, args.description, args.meta)
 
