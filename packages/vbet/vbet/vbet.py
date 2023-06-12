another spot with wrong dim

packages/vbet/vbet/vbet.py
@@ -544,8 +544,9 @@ def vbet_centerlines(in_line_network, in_dem, in_slope, in_hillshade, in_catchme
                     if block['Proximity'].shape == transformed[name].shape:
                         masked_prox = np.ma.MaskedArray(block['Proximity'].data, mask=block['HAND'].mask)
                     else:
+                        dataedit = block['Proximity'].data[:, : transformed[name].shape[1]]
                         maskedit = block['HAND'].mask[:, : transformed[name].shape[1]]
-                        masked_prox = np.ma.MaskedArray(block['Proximity'].data, mask=maskedit)
+                        masked_prox = np.ma.MaskedArray(dataedit, mask=maskedit)
                     if name == 'Slope' and zone < 3:
                         # transformed[name] = transformed[name] - ((np.log(masked_prox + 0.1) + 2.303) / np.log(max_prox + 2.303))
                         transformed[name] = transformed[name] - (np.sqrt(masked_prox) / np.sqrt(max_prox))