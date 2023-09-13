e2c9df835932a944c6bd690a3d7b0d6ba737dfc1
Philip <philip@northarrowresearch.com>
2023-09-13 13:54:19 -0700

    tweaks to scraper as well as project.rs.xml script

--- lib/cybercastor/cybercastor/downloadFiles.py
+++ lib/cybercastor/cybercastor/downloadFiles.py
@@ -3,6 +3,7 @@ Demo script to download files from Data Exchange
 """
 import sys
 import os
+import json
 import traceback
 import argparse
 import datetime
@@ -44,12 +45,11 @@ fields = {
 }
 
 
-def download_files(stage, project_types, hucs, dataset_xml_id, output_db):
+def download_files(stage, project_types, hucs, dataset_xml_id, output_db, reset):
     """[summary]"""
 
-    # Create the output feature class fields. Only those listed here will get copied from the source
-    with GeopackageLayer(output_db, layer_name='vbet_igos', delete_dataset=True) as out_lyr:
-        out_lyr.create_layer(ogr.wkbPoint, epsg=4326, fields=fields)
+    # Update the HUCs that are required
+    hucs_to_process = update_all_hucs_status(output_db, hucs, reset)
 
     riverscapes_api = RiverscapesAPI(stage=stage)
     # Only refresh the token if we need to
@@ -65,41 +65,143 @@ def download_files(stage, project_types, hucs, dataset_xml_id, output_db):
     # project_files_query = riverscapes_api.load_query('projectFiles')
     # project_datasets_query = riverscapes_api.load_query('projectDatasets')
 
-    for project_type in project_types:
-        for huc in hucs:
-            print('Processing HUC {} ({} of {})'.format(huc, hucs.index(huc), len(hucs)))
-            latest_project = get_projects(riverscapes_api, queries['projects'], project_type, huc)
-            if latest_project is None:
-                continue
+    count = 0
+    queued_hucs = True
+    while queued_hucs is True:
+        huc = get_next_huc(output_db)
+        if huc is None:
+            print(f'No HUCs left to process for project type VBET')
+            queued_hucs = False
+            break
 
-            project_id = latest_project['id']
+        count += 1
+        print(f'Processing HUC {huc} ({count} of {hucs_to_process})')
+        latest_project = get_projects(riverscapes_api, queries['projects'], 'VBET', huc)
+        if latest_project is None:
+            update_huc_status(output_db, huc, 'no project')
+            continue
 
-            for local_path, download_file_meta in get_dataset_files(riverscapes_api, queries['files'], queries['datasets'], project_id, dataset_xml_id).items():
-                actual_path = download_project_files(riverscapes_api, os.path.dirname(output_db), project_id, local_path, download_file_meta)
+        project_guid = latest_project['id']
+        project_id = upsert_project(output_db, project_guid, 'VBET', huc, get_project_model_version(latest_project), 'queued')
 
-                process_vbet_gpkg(actual_path, output_db, project_id, local_path)
+        try:
 
-                project_dir = os.path.join(os.path.dirname(output_db), project_id)
+            for local_path, download_file_meta in get_dataset_files(riverscapes_api, queries['files'], queries['datasets'], project_guid, dataset_xml_id).items():
+                actual_path = download_project_files(riverscapes_api, os.path.dirname(output_db), project_guid, local_path, download_file_meta)
+
+                process_vbet_gpkg(actual_path, output_db, project_id)
+
+                project_dir = os.path.join(os.path.dirname(output_db), project_guid)
                 rmtree(project_dir)
 
-            print('here')
-            # project_type_search(local_folder, riverscapes_api, queries, project_type, huc)
+            update_project_status(output_db, project_id, 'complete')
+        except Exception as ex:
+            print('Error processing project')
+            update_project_status(output_db, project_id, 'error')
+
+    print('Processing complete')
 
 
-def process_vbet_gpkg(input_gpkg, output_gpkg, project_id, local_path):
+def get_project_model_version(project_info: Dict) -> str:
+    """Get the model version from the project metadata"""
+
+    if 'meta' in project_info:
+        for meta_item in project_info['meta']:
+            if meta_item['key'].replace(' ', '').lower() == 'modelversion':
+                return meta_item['value']
+
+    return None
+
+
+def update_huc_status(output_gpkg: str, huc: str, status: str) -> None:
+
+    with sqlite3.connect(output_gpkg) as conn:
+        curs = conn.cursor()
+        curs.execute('UPDATE hucs SET status = ? WHERE huc10 = ?', [status, huc])
+        conn.commit()
+
+
+def update_project_status(output_gpkg: str, project_id: str, status: str) -> int:
+    """Update the status of the project"""
+
+    with sqlite3.connect(output_gpkg) as conn:
+        curs = conn.cursor()
+        curs.execute('UPDATE projects SET status = ? WHERE id = ?', [status, project_id])
+        conn.commit()
+
+
+def update_all_hucs_status(output_gpkg: str, hucs: list, reset: bool) -> None:
+    """Set the status of each HUC to required (1) or not required (0)"""
+
+    with sqlite3.connect(output_gpkg) as conn:
+        curs = conn.cursor()
+
+        if reset is True:
+            if len(hucs) < 1:
+                curs.execute("UPDATE hucs SET status = 'queued'")
+            else:
+                curs.execute('UPDATE hucs SET status = NULL')
+                curs.execute("UPDATE hucs SET status = 'queued' WHERE huc10 IN ({})".format(','.join(['?'] * len(hucs))), hucs)
+
+            conn.commit()
+
+        curs.execute("""SELECT count(*)
+                    FROM hucs h
+                        LEFT JOIN projects p ON h.huc10 = p.huc10
+                    WHERE h.status = 'queued'
+                        AND (p.status IS NULL OR p.status = 'error')""")
+        to_process = curs.fetchone()[0]
+
+    print(f'{to_process} HUCs to process')
+    return to_process
+
+
+def get_next_huc(output_gpkg: str) -> str:
+    """Get the next HUC to process"""
+
+    with sqlite3.connect(output_gpkg) as conn:
+        curs = conn.cursor()
+        curs.execute("""SELECT h.huc10, p.id, p.guid, p.status
+                    FROM hucs h
+                        LEFT JOIN projects p ON h.huc10 = p.huc10
+                    WHERE h.status = 'queued'
+                        AND (p.status IS NULL OR p.status = 'error')
+                    ORDER BY h.huc10 limit 1""")
+
+        huc = curs.fetchone()
+
+    if huc is None:
+        return None
+    else:
+        return huc[0]
+
+
+def upsert_project(output_gpkg: str, project_guid: str, project_type: str, huc: str, model_version: str, status: str) -> int:
+
+    with sqlite3.connect(output_gpkg) as conn:
+        curs = conn.cursor()
+        curs.execute("""INSERT INTO projects (guid, huc10, project_type, model_version, status)
+                    VALUES (?, ?, ?, ?, ?) ON CONFLICT (guid) DO UPDATE SET status = ?""", [project_guid, huc, project_type, model_version, status, status])
+
+        curs.execute('SELECT id FROM projects WHERE guid = ?', [project_guid])
+        project_id = curs.fetchone()[0]
+        conn.commit()
+
+    return project_id
+
+
+def process_vbet_gpkg(input_gpkg, output_gpkg, project_id: int):
+    """Note project ID is the SQLite integer ID, not the GUID string"""
 
     with GeopackageLayer(input_gpkg, layer_name='vbet_igos') as input_lyr:
-        with GeopackageLayer(output_db, layer_name='vbet_igos', write=True) as output_lyr:
+        with GeopackageLayer(output_gpkg, layer_name='vbet_igos', write=True) as output_lyr:
 
             for in_feature, _counter, _progbar in input_lyr.iterate_features():
                 field_values = {field: in_feature.GetField(field) for field in fields}
                 field_values['project_id'] = project_id
 
                 output_lyr.create_feature(in_feature.GetGeometryRef(), field_values)
-  
 
-
- 
 
 def get_projects(riverscapes_api, projects_query, project_type: str, huc: str) -> Dict:
     """[summary]"""
@@ -132,10 +234,10 @@ def get_projects(riverscapes_api, projects_query, project_type: str, huc: str) -
         project_versions = {}
         for project_id, project_info in projects.items():
             for key, val in {meta_item['key']: meta_item['value'] for meta_item in project_info['meta']}.items():
-                if key.replace(' ','').lower() == 'modelversion' and val is not None:
+                if key.replace(' ', '').lower() == 'modelversion' and val is not None:
                     project_versions[semantic_version.Version(val)] = project_id
                     break
-            
+
         project_versions_list = list(project_versions)
         project_versions_list.sort(reverse=True)
         return projects[project_versions[project_versions_list[0]]]
@@ -144,9 +246,9 @@ def get_projects(riverscapes_api, projects_query, project_type: str, huc: str) -
 def get_dataset_files(riverscapes_api, files_query, datasets_query, project_id: str, dataset_xml_id: str) -> Dict:
 
     # Build a dictionary of files in the project keyed by local path to downloadUrl
-    file_results = riverscapes_api.run_query(files_query, {"projectId": project_id}) 
+    file_results = riverscapes_api.run_query(files_query, {"projectId": project_id})
     files = {file['localPath']: file for file in file_results['data']['project']['files']}
-    
+
     dataset_limit = 500
     dataset_offset = 0
     dataset_total = 0
@@ -155,7 +257,7 @@ def get_dataset_files(riverscapes_api, files_query, datasets_query, project_id: 
     datasets = {}
     while dataset_offset == 0 or dataset_offset < dataset_total:
 
-        dataset_results = riverscapes_api.run_query(datasets_query, {"projectId": project_id, "limit": dataset_limit, "offset": dataset_offset}) 
+        dataset_results = riverscapes_api.run_query(datasets_query, {"projectId": project_id, "limit": dataset_limit, "offset": dataset_offset})
 
         project_datasets = dataset_results['data']['project']['datasets']
         dataset_total = project_datasets['total']
@@ -180,55 +282,73 @@ def download_project_files(riverscapes_api, local_folder, project_id, local_path
     return download_path
 
 
-# def project_type_search(local_folder, riverscapes_api, queries, project_type: str, huc: str):
-#     """[summary]"""
+def build_output(output_gpkg: str, huc10_attributes: str, reset: bool) -> None:
+    """Build the output GeoPackage and feature class fields
+    or optionally purge the existing output and start from scratch"""
 
-#     # Add items to this dictionary to filter which projects are retrieved.
-#     # For example, to only get projects for a certain HUC, project type or model version etc.
-#     search_params = {
-#         'projectTypeId': project_type,
-#         'meta': [{
-#             'key': 'HUC',
-#             'value': huc
-#         }]
-#     }
+    if os.path.isfile(output_gpkg):
+        if reset is True:
+            with sqlite3.connect(output_gpkg) as conn:
+                curs = conn.cursor()
+                curs.execute('DELETE FROM vbet_igos')
+                conn.commit()
+        return
 
-#     project_limit = 500
-#     project_offset = 0
-#     total = 0
-#     while project_offset == 0 or project_offset < total:
-#         results = riverscapes_api.run_query(queries['projects'], {"searchParams": search_params, "limit": project_limit, "offset": project_offset})
-#         total = results['data']['searchProjects']['total']
-#         project_offset += project_limit
+    # Create the output Geopackage and feature class fields and include the project_id
+    vbet_igo_fields = {'project_id': ogr.OFTInteger}
+    vbet_igo_fields.update(fields)
+    with GeopackageLayer(output_gpkg, layer_name='vbet_igos', delete_dataset=True) as out_lyr:
+        out_lyr.create_layer(ogr.wkbPoint, epsg=4326, fields=vbet_igo_fields)
 
-#         projects = results['data']['searchProjects']['results']
+    out_lyr = None
 
-#         for search_result in projects:
+    # Create lookup table
+    with sqlite3.connect(output_gpkg) as conn:
+        curs = conn.cursor()
+        curs.execute('CREATE TABLE IF NOT EXISTS hucs (huc10 TEXT PRIMARY KEY, name TEXT, states TEXT, areasqkm REAL, status TEXT)')
 
-#             project = search_result['item']
+        # Load HUC 10 attributes
+        huc_data = json.load(open(huc10_attributes, encoding='utf-8'))
+        curs.executemany('INSERT INTO hucs (huc10, name, states, areasqkm) VALUES (?, ?, ?, ?)', [(huc['HUC10'], huc['NAME'], huc['STATES'], huc['AREASQKM']) for huc in huc_data])
 
-#             # Build a dictionary of files in the project keyed by local path to downloadUrl
-#             file_results = riverscapes_api.run_query(queries['files'], {"projectId": project['id']}) 
-#             files = {file['localPath']: file['downloadUrl'] for file in file_results['data']['project']['files']}
-            
-#             dataset_limit = 500
-#             dataset_offset = 0
-#             dataset_total = 0
+        # Index the HUCS in one go
+        curs.execute('CREATE UNIQUE INDEX IF NOT EXISTS hucs_huc10_idx ON hucs (huc10)')
 
-#             # Query for datasets in the project. Then loop over the files within the dataset
-#             datasets = {}
-#             while dataset_offset == 0 or dataset_offset < dataset_total:
+        # Create table to track projects
+        curs.execute('CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY, guid TEXT NOT NULL, huc10 TEXT, project_type TEXT, model_version TEXT, status TEXT, metadata TEXT)')
+        curs.execute('CREATE INDEX IF NOT EXISTS projects_huc10_idx ON projects (huc10)')
+        curs.execute('CREATE UNIQUE INDEX IF NOT EXISTS projects_guid_idx ON projects (guid)')
 
-#                 dataset_results = riverscapes_api.run_query(queries['datasets'], {"projectId": project['id'], "limit": dataset_limit, "offset": dataset_offset}) 
+        # Index the project ID in the vbet_igos table
+        curs.execute('CREATE INDEX IF NOT EXISTS vbet_igos_project_id_idx ON vbet_igos (project_id)')
 
-#                 project_datasets = dataset_results['data']['project']['datasets']
-#                 dataset_total = project_datasets['total']
-#                 dataset_offset += dataset_limit
+        # Create a summary view of all the VBET metrics
+        field_list = []
+        for process in ['min', 'max', 'avg']:
+            for field, data_type in fields.items():
+                if data_type != ogr.OFTReal:
+                    continue
+                field_list.append(f'{process}({field}) AS {process}_{field}')
 
-#                 for dataset in project_datasets['items']:
-#                     datasets[dataset['datasetXMLId']] = dataset['localPath']
+        view_name = 'vw_huc_summary_stats'
+        huc_polygons = 'Huc10_conus'
+        curs.execute(f"""CREATE VIEW {view_name} AS
+                     SELECT
+                        h.fid,
+                        h.geom,
+                        {', '.join(field_list)}
+                    FROM {huc_polygons} h
+                        INNER JOIN projects p on h.HUC10 = p.huc10
+                        INNER JOIN vbet_igos vi on p.id = vi.project_id
+                    GROUP BY h.fid, h.geom""")
 
-#             print('here')
+        curs.execute("""INSERT INTO gpkg_contents (table_name, data_type, identifier, min_x, min_y, max_x, max_y, srs_id)
+                     SELECT ?, features, ?, min_x, min_y, max_x, max_y, srs_id FROM gpkg_contents WHERE table_name = ?""", [view_name, view_name, huc_polygons])
+
+        curs.execute("""INSERT INTO gpkg_geometry_columns (table_name, column_name, geometry_type_name, srs_id, z, m)
+                     SELECT ?, 'geom', geometry_type_name, srs_id, z, m FROM gpkg_geometry_columns WHERE table_name = ?""", [view_name, huc_polygons])
+
+        conn.commit()
 
 
 if __name__ == '__main__':
@@ -238,20 +358,24 @@ if __name__ == '__main__':
     parser.add_argument('HUCs', help='Comma separated list of HUC codes', type=str)
     parser.add_argument('dataset_xml_id', help='Dataset XMLId to download', type=str)
     parser.add_argument('local_folder', help='Top level folder where to download files', type=str)
+    parser.add_argument('output_gpkg', help='Path where to existing output Geopackage or where it will be created', type=str)
+    parser.add_argument('huc10_attributes', help='Name of JSON file in local_folder that contains HUC 10 definitions', type=str)
     parser.add_argument('--verbose', help='(optional) a little extra logging ', action='store_true', default=False)
+    parser.add_argument('--reset', help='(optional) delete all existing outputs before running and run from scratch', action='store_true', default=False)
     args = dotenv.parse_args_env(parser)
 
     project_types = args.project_types.split(',')
-    hucs = args.HUCs.split(',')
+    hucs = list() if args.HUCs == '.' else args.HUCs.split(',')
 
     safe_makedirs(args.local_folder)
-    output_db = os.path.join(args.local_folder, f'output_{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M")}.sqlite')
+    # output_db = os.path.join(args.local_folder, f'output_{datetime.datetime.now().strftime("%Y_%m_%d_%H_%M")}.sqlite')
 
-    try:
-        download_files(args.stage, project_types, hucs, args.dataset_xml_id, output_db)
+    # try:
+    build_output(args.output_gpkg, args.huc10_attributes, args.reset)
+    download_files(args.stage, project_types, hucs, args.dataset_xml_id, args.output_gpkg, args.reset)
 
-    except Exception as e:
-        traceback.print_exc(file=sys.stdout)
-        sys.exit(1)
+    # except Exception as e:
+    #     traceback.print_exc(file=sys.stdout)
+    #     sys.exit(1)
 
     sys.exit(0)
---
+++ lib/cybercastor/cybercastor/scraper_project.py
@@ -0,0 +1,110 @@
+"""Script to create a project XML file for the VBET synthesis GeoPackage."""
+import os
+import json
+import argparse
+import sqlite3
+from datetime import datetime
+from rscommons import dotenv
+from rsxml.project_xml import Project, ProjectBounds, Coords, BoundingBox, Realization, MetaData, Geopackage, GeopackageLayer, GeoPackageDatasetTypes
+
+
+def create_project_file(gpkg_path: str) -> None:
+    """Creates a project XML file for the VBET synthesis GeoPackage.
+    gpkg_path: Path to the VBET synthesis GeoPackage
+    output_path: Output path to project XML
+    """
+
+    meta = MetaData()
+    with sqlite3.connect(gpkg_path) as conn:
+        curs = conn.cursor()
+        meta.add_meta('Number of IGOs', get_db_statistic(curs, 'SELECT count(*) FROM vbet_igos'))
+        meta.add_meta('Number of HUCs', get_db_statistic(curs, 'SELECT count(*) FROM hucs'))
+        meta.add_meta('Number of Projects', get_db_statistic(curs, 'SELECT count(*) FROM projects'))
+
+        # Build a bounding box from the HUCs
+        curs.execute('select min_x, min_y, max_x, max_y, srs_id from gpkg_contents WHERE table_name = ?', ['vbet_igos'])
+        coords = curs.fetchone()
+        centroid = Coords((coords[0] + coords[2]) / 2, (coords[1] + coords[3]) / 2)
+        polygon = [
+            [coords[0], coords[1]],
+            [coords[0], coords[3]],
+            [coords[2], coords[3]],
+            [coords[2], coords[1]],
+            [coords[0], coords[1]]
+        ]
+        bbox = BoundingBox(coords[0], coords[1], coords[2], coords[3])
+        meta.add_meta('SRS ID', coords[4])
+
+        geojson = {'type': 'FeatureCollection', 'features': [{
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {
+                'type': 'Polygon',
+                'coordinates': [polygon]
+            }
+        }]}
+
+    # Create the GeoJSON file
+    geojson_path = os.path.join(os.path.dirname(gpkg_path), 'project_bounds.geojson')
+    with open(geojson_path, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(geojson))
+    print(f'GeoJSON written to: {geojson_path}')
+
+    project = Project(
+        name='VBET Synthesis Version 3.1.0',
+        proj_path='project.rs.xml',
+        project_type='VBETSynthesis',
+        description='Synthesises together results from the latest version of VBET for each 10 digit HUC from the Mississippi River Basin to the US west coast.'
+        ' Most VBET projects in this synthesis are version 3.1.0, but some are lower versions. This synthesis only combines IGOs and their corresponding VBET metrics.'
+        ' It does not include any other data from the source VBET projects.',
+        bounds=ProjectBounds(
+            centroid=centroid,
+            bounding_box=bbox,
+            filepath='project_bounds.json',
+        ),
+        meta_data=meta,
+        realizations=[
+            Realization(
+                xml_id='vbet_synthesis_realization_01',
+                name='Realization',
+                product_version='0.0.1',
+                date_created=datetime(2023, 9, 12), datasets=[
+                    Geopackage(
+                        xml_id='vbet_synth_geopackage',
+                        name='Synthesis GeoPackage',
+                        path=os.path.basename(gpkg_path),
+                        description='The one and only GeoPackage produced by the synthesis. There are two features in this GeoPackage, one for the IGOs and one for the 10 digit HUCs.'
+                        ' There are two additional non-spatial tables: projects that contains a list of all the VBET projects scraped. HUCs is another list of all the 10 digit HUCs in CONUS.'
+                        ' These latter two tables are used to track processing and to ensure that all HUCs are accounted for. There is also a spatial view that joins the IGOs to the HUCs. This can be used for quick visualization of the data.',
+                        layers=[
+                            GeopackageLayer(lyr_name='vw_huc_summary_stats',
+                                            name='HUC summary statistics',
+                                            ds_type=GeoPackageDatasetTypes.VECTOR,
+                                            description='Min, Max and Mean values for each VBET metric for each HUC',
+                                            )
+                        ]
+                    )
+                ]
+            )
+        ]
+    )
+
+    # Write it to disk
+    output_path = os.path.join(os.path.dirname(gpkg_path), 'project.rs.xml')
+    project.write(output_path)
+
+    print(f'Process complete. Output written to: {output_path}')
+
+
+def get_db_statistic(curs: sqlite3.Cursor, sql: str) -> int or float:
+    """Retrieve the value from a SQL query."""
+    curs.execute(sql)
+    return curs.fetchone()[0]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('gpkg_path', help='Path to the VBET synthesis GeoPackage', type=str)
+    args = dotenv.parse_args_env(parser)
+
+    create_project_file(args.gpkg_path)
