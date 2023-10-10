a3a05ce68f08bd5f952cf7605d5121805274afed
Jordan Gilbert <jtgilbert89@gmail.com>
2023-10-10 10:56:27 -0600

    dgo-based metrics

--- packages/rme/rme/database/metrics_schema.sql
+++ packages/rme/rme/database/metrics_schema.sql
@@ -19,9 +19,9 @@ CREATE TABLE metric_values (
     metric_value REAL,
     metadata TEXT,
     qaqc_date TEXT,
-    PRIMARY KEY (point_id, metric_id)
+    PRIMARY KEY (dgo_id, metric_id)
 
-    CONSTRAINT fk_point_id FOREIGN KEY (point_id) REFERENCES vbet_segments (fid),
+    CONSTRAINT fk_point_id FOREIGN KEY (dgo_id) REFERENCES vbet_segments (fid),
     CONSTRAINT fk_metric_id FOREIGN KEY (metric_id) REFERENCES metrics (metric_id)
 );
 
@@ -52,16 +52,21 @@ CREATE TABLE measurement_values (
     measurement_value REAL,
     metadata TEXT,
     qaqc_date TEXT,
-    PRIMARY KEY (point_id, measurement_id)
+    PRIMARY KEY (dgo_id, measurement_id)
 
-    CONSTRAINT fk_point_id FOREIGN KEY (point_id) REFERENCES points (fid),
+    CONSTRAINT fk_point_id FOREIGN KEY (dgo_id) REFERENCES vbet_segments (fid),
     CONSTRAINT fk_measurement_id FOREIGN KEY (measurement_id) REFERENCES measurements (measurement_id)
 );
 
-CREATE INDEX ix_metric_values ON metric_values (point_id);
+CREATE INDEX ix_metric_values ON metric_values (dgo_id);
+CREATE INDEX ix_active_fp_area ON vbet_segments (active_floodplain_area);
+CREATE INDEX ix_active_chan_area ON vbet_segments (active_channel_area);
+CREATE INDEX ix_active_seg_area ON vbet_segments (segment_area);
+CREATE INDEX ix_active_center_length ON vbet_segments (centerline_length);
+CREATE INDEX ix_fp_area ON vbet_segments (floodplain_area);
 
 INSERT INTO gpkg_contents (table_name, data_type) VALUES ('metric_values', 'attributes');
 INSERT INTO gpkg_contents (table_name, data_type) VALUES ('metrics', 'attributes');
+INSERT INTO gpkg_contents (table_name, data_type) VALUES ('igo_metric_values', 'attributes');
 INSERT INTO gpkg_contents (table_name, data_type) VALUES ('measurement_values', 'attributes');
 INSERT INTO gpkg_contents (table_name, data_type) VALUES ('measurements', 'attributes');
-
