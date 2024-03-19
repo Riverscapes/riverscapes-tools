fd4e79f28eedd93abc3f924a8a5924a70bbb5e31
Jordan Gilbert <jtgilbert89@gmail.com>
2024-03-19 21:12:02 +0000

    tested one huc

--- packages/confinement/confinement/utils/continuous_line.py
+++ packages/confinement/confinement/utils/continuous_line.py
@@ -10,21 +10,25 @@ def continuous_line(line):
         return line
 
     line_segs = list(line.geoms)
+    comp_dist = min([seg.length for seg in line_segs])/4
 
     start_points = []
     end_points = []
     for geom in line:
         start_points.append(geom.coords[0])
         end_points.append(geom.coords[-1])
-    # st_dists = {pnt: [] for pnt in start_points}
-    # for s_pnt in start_points:
-    #     for e_pnt in end_points:
-    #         st_dists[s_pnt].append(Point(s_pnt).distance(Point(e_pnt)))
-    # start_dists = {k: min(v) for k, v in st_dists.items()}
-    # pnt_start = max(start_dists, key=start_dists.get)
 
     end_dists = {pnt: [] for pnt in end_points}
     for e_pnt in end_points:
         for s_pnt in start_points:
             end_dists[e_pnt].append(Point(e_pnt).distance(Point(s_pnt)))
     end_dists = {k: min(v) for k, v in end_dists.items()}
+
+    for ept, dist in end_dists.items():
+        if dist > 0 and dist < comp_dist:
+            for spt in start_points:
+                if Point(ept).distance(Point(spt)) == dist:
+                    line_segs.append(LineString([ept, spt]))
+                    break
+
+    return linemerge(MultiLineString(line_segs))
