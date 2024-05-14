eac5cf80b0368b19c0798d89dcd041a1d9de36b6
Rowan G <80495206+r-k-g@users.noreply.github.com>
2024-05-14 23:03:59 +0000

    Channelarea report improvements: more units in table, restore broken text

--- packages/channel/channel/channel_report.py
+++ packages/channel/channel/channel_report.py
@@ -37,10 +37,9 @@ class ChannelReport(RSReport):
 
         self.out_section = self.section('Outputs', 'Outputs')
         p1 = ET.Element('p')
-        p1.text = 'The Channel Area Tool is a simple tool for aggregating polygons from different data sources into a single feature class. '
-        'The output of the Channel Area Tool is a polygon layer representing the bankfull width of a channel network. See documentation for the tool '
-        aEl1 = ET.SubElement(p1, 'a', {'href': 'https://tools.riverscapes.net/channel/'})
-        aEl1.text = 'here.'
+        p1.text = 'The Channel Area Tool is a simple tool for generating polygons representing the spatial extent of the drainage network within a watershed. The primary purpose for the tool is that the outputs it produces are used as inputs in other Riverscapes tools. Geospatial tools often use a simple line network to represent streams. Depending on the functions a tool is performing, this can be problematic as a line can represent both a narrow, first order stream as well as large, wide rivers. Many Riverscapes tools analyze areas outside of the channel (for example, to look at streamside vegetation), therefore an accurate representation of the actual channel, not simply a line, is necessary. The tool is comprised of a simple algorithm for combining polygons representing channels with polygons derived from attributes on a drainage network (line). As long as a drainage network has an attribute recording the upstream contributing drainage area for each segment, regional relationships relating channel width to drainage area can be used to buffer the channel segments, and the resulting polygons can be merged with any other available polygons. This gives a first order approximation of the active channel area. As channels are active and constantly moving through time, greater accuracy can be achieved with more recent, high resolution datasets, or with user input (e.g., editing channel positions or channel polygons). For more information, refer to '
+        aEl1 = ET.SubElement(p1, 'a', {'href': 'https://tools.riverscapes.net/channelarea/'})
+        aEl1.text = 'documentation.'
         self.out_section.append(p1)
         self.outputs_content()
 
@@ -49,6 +48,8 @@ class ChannelReport(RSReport):
         p2in = ET.Element('p')
         p1in.text = 'The inputs to the Channel Area tool are a drainage network (polyline) layer, and polygons representing river channels.'
         p2in.text = 'The default inputs for the tool are the NHD Flowline, NHD Area, and NHD Waterbody feature classes.'
+        in_section.append(p1in)
+        in_section.append(p2in)
 
         self.layersummary("Inputs", "Inputs")
         self.layersummary("Intermediates", "Intermediates")
@@ -65,6 +66,47 @@ class ChannelReport(RSReport):
                 self.layerprint(lyr, section, self.project_root)
             if lyr.tag in ['SQLiteDB']:
                 self.layerprint(lyr, section, self.project_root)
+
+    def custom_table(self, table_contents, el_parent, header_names=None):
+        table = ET.Element('table')
+
+        if header_names is not None:
+            thead = ET.Element('thead')
+            theadrow = ET.Element('tr')
+            thead.append(theadrow)
+            table.append(thead)
+
+            for name in header_names:
+                th = ET.Element('th')
+                th.text = name
+                theadrow.append(th)
+
+        tbody = ET.Element('tbody')
+        table.append(tbody)
+
+        for key, val in table_contents.items():
+
+            tr = ET.Element('tr')
+            tbody.append(tr)
+
+            th = ET.Element('th')
+            th.text = key
+            tr.append(th)
+
+            for item in val:
+                # If the value is a URL, make it a link
+                if isinstance(item, str) and item.startswith("http"):
+                    td = ET.Element('td', attrib={'class': 'text url'})
+                    a = ET.Element('a', attrib={'href': item})
+                    a.text = item
+                    td.append(a)
+                else:
+                    item, class_name = RSReport.format_value(item)
+                    td = ET.Element('td', attrib={'class': class_name})
+                    td.text = item
+                tr.append(td)
+
+        el_parent.append(table)
 
     def outputs_content(self):
 
@@ -107,14 +149,25 @@ class ChannelReport(RSReport):
                     poly_areas[lyr_label] = float('{:.2f}'.format(area))
 
         section = self.section('AreaBreakdown', 'Data Source Breakdown', el_parent=self.out_section, level=2)
-        table_dict = {lyr_label_dict[i]: v for i, v in poly_areas.items()}
-        RSReport.create_table_from_dict(table_dict, section)
+        table_contents = {
+            lyr_label_dict[i]: [v, v / 4046.8564, v / 10000]
+            for i, v in poly_areas.items()
+        }
+        self.custom_table(table_contents, section, header_names=['Data Source', 'Area (m^2)', 'Area (acres)', 'Area (hectares)'])
+
         pie_path = os.path.join(self.images_dir, 'area_breakdown.png')
-        pie([values for key, values in poly_areas.items()], [lyr_label_dict[key] for key in poly_areas.keys()], "title", [self.colors[key] for key in poly_areas.keys()], pie_path)
+        pie(
+            [values for key, values in poly_areas.items()],
+            [lyr_label_dict[key] for key in poly_areas],
+            "",
+            [self.colors[key] for key in poly_areas],
+            pie_path
+        )
+
         plot_wrapper = ET.Element('div', attrib={'class': 'plots'})
         img_wrap = ET.Element('div', attrib={'class': 'imgWrap'})
         img = ET.Element('img', attrib={
-            'src': '{}/{}'.format(os.path.basename(self.images_dir), os.path.basename(pie_path)),
+            'src': os.path.join(os.path.basename(self.images_dir), os.path.basename(pie_path)),
             'alt': 'pie_chart'
         })
         img_wrap.append(img)
