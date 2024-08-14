d0f35a55fd33ec77a94425f6e8797081c31c07f7
Jordan Gilbert <jtgilbert89@gmail.com>
2024-08-14 17:07:09 +0000

    filled out EI table

--- packages/brat/sqlbrat/capacity_validation.py
+++ packages/brat/sqlbrat/capacity_validation.py
@@ -74,31 +74,92 @@ def electivity_index(gpkg_path: str):
         total_dams = db.curs.fetchone()['dams']
         db.curs.execute('SELECT SUM(iGeo_Len) As len FROM vwReaches')
         total_length = db.curs.fetchone()['len']
-        db.curs.execute('SELECT SUM(dam_count) AS dc, SUM(length) AS sl FROM dam_counts WHERE predicted_capacity = 0')
+        db.curs.execute('SELECT SUM(dam_count) AS dc, SUM(length) AS sl, SUM(predicted_capacity * (length/1000)) AS cap FROM dam_counts WHERE predicted_capacity = 0')
         none_cap = db.curs.fetchone()
-        none_ei = (none_cap['dc'] / total_dams) / (none_cap['sl'] / total_length)
-        db.curs.execute('SELECT SUM(dam_count) AS dc, SUM(length) AS sl FROM dam_counts WHERE predicted_capacity > 0 and predicted_capacity <= 1')
+        none_len = none_cap['sl']
+        none_ct = none_cap['dc']
+        none_predcap = none_cap['cap']
+        none_percap = none_ct / none_predcap if none_predcap > 0 else 'NA'
+        none_ei = (none_ct / total_dams) / (none_len / total_length)
+        db.curs.execute('SELECT SUM(dam_count) AS dc, SUM(length) AS sl, SUM(predicted_capacity * (length/1000)) AS cap FROM dam_counts WHERE predicted_capacity > 0 and predicted_capacity <= 1')
         rare_cap = db.curs.fetchone()
-        rare_ei = (rare_cap['dc'] / total_dams) / (rare_cap['sl'] / total_length)
-        db.curs.execute('SELECT SUM(dam_count) AS dc, SUM(length) AS sl FROM dam_counts WHERE predicted_capacity > 1 and predicted_capacity <= 5')
+        rare_len = rare_cap['sl']
+        rare_ct = rare_cap['dc']
+        rare_predcap = rare_cap['cap']
+        rare_percap = rare_ct / rare_predcap if rare_predcap > 0 else 'NA'
+        rare_ei = (rare_ct / total_dams) / (rare_len / total_length)
+        db.curs.execute('SELECT SUM(dam_count) AS dc, SUM(length) AS sl, SUM(predicted_capacity * (length/1000)) AS cap FROM dam_counts WHERE predicted_capacity > 1 and predicted_capacity <= 5')
         occ_cap = db.curs.fetchone()
-        occ_ei = (occ_cap['dc'] / total_dams) / (occ_cap['sl'] / total_length)
-        db.curs.execute('SELECT SUM(dam_count) AS dc, SUM(length) AS sl FROM dam_counts WHERE predicted_capacity > 5 and predicted_capacity <= 15')
+        occ_len = occ_cap['sl']
+        occ_ct = occ_cap['dc']
+        occ_predcap = occ_cap['cap']
+        occ_ei = (occ_ct / total_dams) / (occ_len / total_length)
+        occ_percap = occ_ct / occ_predcap if occ_predcap > 0 else 'NA'
+        db.curs.execute('SELECT SUM(dam_count) AS dc, SUM(length) AS sl, SUM(predicted_capacity * (length/1000)) AS cap FROM dam_counts WHERE predicted_capacity > 5 and predicted_capacity <= 15')
         freq_cap = db.curs.fetchone()
-        freq_ei = (freq_cap['dc'] / total_dams) / (freq_cap['sl'] / total_length)
-        db.curs.execute('SELECT SUM(dam_count) AS dc, SUM(length) AS sl FROM dam_counts WHERE predicted_capacity > 15')
+        freq_len = freq_cap['sl']
+        freq_ct = freq_cap['dc']
+        freq_predcap = freq_cap['cap']
+        freq_percap = freq_ct / freq_predcap if freq_predcap > 0 else 'NA'
+        freq_ei = (freq_ct / total_dams) / (freq_len / total_length)
+        db.curs.execute('SELECT SUM(dam_count) AS dc, SUM(length) AS sl, SUM(predicted_capacity * (length/1000)) AS cap FROM dam_counts WHERE predicted_capacity > 15')
         perv_cap = db.curs.fetchone()
-        perv__ei = (perv_cap['dc'] / total_dams) / (perv_cap['sl'] / total_length)
+        perv_len = perv_cap['sl']
+        perv_ct = perv_cap['dc']
+        perv_predcap = perv_cap['cap']
+        perv_percap = perv_ct / perv_predcap if perv_predcap > 0 else 'NA'
+        perv_ei = (perv_ct / total_dams) / (perv_len / total_length)
 
     with open(out_path, 'w', newline='') as csvfile:
-        fieldnames = ['Capcacity', 'EI']
+        fieldnames = ['Capcacity', 'Stream Length', 'Percent of Drainage Network', 'Surveyed Dams', 'BRAT Estimated Capacity',
+                      'Average Surveyed Dam Density', 'Average Predicted Capacity', 'Percent of Modeled Capacity', 'EI']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        writer.writerow({'Capcacity': 'None', 'EI': none_ei})
-        writer.writerow({'Capcacity': 'Rare', 'EI': rare_ei})
-        writer.writerow({'Capcacity': 'Occasional', 'EI': occ_ei})
-        writer.writerow({'Capcacity': 'Frequent', 'EI': freq_ei})
-        writer.writerow({'Capcacity': 'Pervasive', 'EI': perv__ei})
+        writer.writerow({'Capcacity': 'None',
+                         'Stream Length': none_len,
+                         'Percent of Drainage Network': (none_len/total_length)*100,
+                         'Surveyed Dams': none_ct,
+                         'BRAT Estimated Capacity': none_predcap,
+                         'Average Surveyed Dam Density': none_ct / (none_len/1000),
+                         'Average Predicted Capacity': none_predcap / (none_len/1000),
+                         'Percent of Modeled Capacity': none_percap,
+                         'EI': none_ei})
+        writer.writerow({'Capcacity': 'Rare',
+                         'Stream Length': rare_len,
+                         'Percent of Drainage Network': (rare_len/total_length)*100,
+                         'Surveyed Dams': rare_ct,
+                         'BRAT Estimated Capacity': rare_predcap,
+                         'Average Surveyed Dam Density': rare_ct / (rare_len/1000),
+                         'Average Predicted Capacity': rare_predcap / (rare_len/1000),
+                         'Percent of Modeled Capacity': rare_percap,
+                         'EI': rare_ei})
+        writer.writerow({'Capcacity': 'Occasional',
+                         'Stream Length': occ_len,
+                         'Percent of Drainage Network': (occ_len/total_length)*100,
+                         'Surveyed Dams': occ_ct,
+                         'BRAT Estimated Capacity': occ_predcap,
+                         'Average Surveyed Dam Density': occ_ct / (occ_len/1000),
+                         'Average Predicted Capacity': occ_predcap / (occ_len/1000),
+                         'Percent of Modeled Capacity': occ_percap,
+                         'EI': occ_ei})
+        writer.writerow({'Capcacity': 'Frequent',
+                         'Stream Length': freq_len,
+                         'Percent of Drainage Network': (freq_len/total_length)*100,
+                         'Surveyed Dams': freq_ct,
+                         'BRAT Estimated Capacity': freq_predcap,
+                         'Average Surveyed Dam Density': freq_ct / (freq_len/1000),
+                         'Average Predicted Capacity': freq_predcap / (freq_len/1000),
+                         'Percent of Modeled Capacity': freq_percap,
+                         'EI': freq_ei})
+        writer.writerow({'Capcacity': 'Pervasive',
+                         'Stream Length': perv_len,
+                         'Percent of Drainage Network': (perv_len/total_length)*100,
+                         'Surveyed Dams': perv_ct,
+                         'BRAT Estimated Capacity': perv_predcap,
+                         'Average Surveyed Dam Density': perv_ct / (perv_len/1000),
+                         'Average Predicted Capacity': perv_predcap / (perv_len/1000),
+                         'Percent of Modeled Capacity': perv_percap,
+                         'EI': perv_ei})
 
 
 def validation_plots(brat_gpkg_path: str):
@@ -152,7 +213,7 @@ def validation_plots(brat_gpkg_path: str):
 
     fig, ax = plt.subplots()
     ax.scatter(obs_pred[1], obs_pred[0], marker='x', c='darkblue')
-    # ax.plot([0, max(obs_pred[1])], [0, max(obs_pred[1])], 'r', label='1:1')
+    ax.plot([0, max(obs_pred[1])], [0, max(obs_pred[1])], 'r', label='1:1')
     ax.plot(xdata, res90.intercept + res90.slope * xdata, 'k', linestyle='-.', label='90th Percentile')
     ax.plot(xdata, res75.intercept + res75.slope * xdata, 'k', linestyle=':', label='75th Percentile')
     ax.plot(xdata, res.intercept + res.slope * xdata, 'k', linestyle='--', label='50th Percentile')
