b94c53ee41032625fc10830cd071b51d2d8033cb
Philip <philip@northarrowresearch.com>
2024-08-23 13:26:13 -0700

    rme schema improvements

--- packages/rme/rme/database/metrics_schema.sql
+++ packages/rme/rme/database/metrics_schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE metrics (
     metric_id INTEGER PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
-    machine_code TEXT NOT NULL,
-    data_type TEXT,
+    name TEXT UNIQUE NOT NULL,
+    machine_code TEXT UNIQUE NOT NULL,
+    data_type TEXT NOT NULL,
     field_name TEXT,
     description TEXT,
     method TEXT,
@@ -40,9 +40,9 @@ CREATE TABLE igo_metric_values (
 
 CREATE TABLE measurements (
     measurement_id INTEGER PRIMARY KEY NOT NULL,
-    name TEXT,
-    machine_code TEXT,
-    data_type TEXT,
+    name TEXT UNIQUE NOT NULL,
+    machine_code TEXT UNIQUE NOT NULL,
+    data_type TEXT NOT NULL,
     description TEXT,
     is_active INTEGER
 );
@@ -54,18 +54,14 @@ CREATE TABLE measurement_values (
     metadata TEXT,
     qaqc_date TEXT,
     PRIMARY KEY (dgo_id, measurement_id)
-
-    CONSTRAINT fk_point_id FOREIGN KEY (dgo_id) REFERENCES dgos (fid),
-    CONSTRAINT fk_measurement_id FOREIGN KEY (measurement_id) REFERENCES measurements (measurement_id)
+    
+    CONSTRAINT fk_measurement_values_dgo_id FOREIGN KEY (dgo_id) REFERENCES dgos (fid),
+    CONSTRAINT fk_measurement_values_measurement_id FOREIGN KEY (measurement_id) REFERENCES measurements (measurement_id)
 );
+CREATE INDEX fx_measurement_values_measurement_id ON measurement_values (measurement_id);
 
 CREATE INDEX ix_dgo_metric_values_metric_id ON dgo_metric_values (metric_id);
 CREATE INDEX ix_igo_metric_values_metric_id ON igo_metric_values (metric_id);
-CREATE INDEX ix_active_fp_area ON dgos (low_lying_floodplain_area);
-CREATE INDEX ix_active_chan_area ON dgos (active_channel_area);
-CREATE INDEX ix_active_seg_area ON dgos (segment_area);
-CREATE INDEX ix_active_center_length ON dgos (centerline_length);
-CREATE INDEX ix_fp_area ON dgos (floodplain_area);
 
 INSERT INTO gpkg_contents (table_name, data_type) VALUES ('dgo_metric_values', 'attributes');
 INSERT INTO gpkg_contents (table_name, data_type) VALUES ('metrics', 'attributes');
