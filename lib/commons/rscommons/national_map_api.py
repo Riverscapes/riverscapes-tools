6e19af66b6280e834779e5aea81edd5126905512
Matt Reimer <matt@northarrowresearch.com>
2024-09-05 08:49:23 -0700

    national map logging fix

--- lib/commons/rscommons/national_map_api.py
+++ lib/commons/rscommons/national_map_api.py
@@ -1,6 +1,6 @@
 import json
 import requests
-
+import urllib.parse
 from rscommons import Logger
 
 
@@ -20,17 +20,36 @@ class TNM:
         params["outputFormat"] = "JSON"
 
         log = Logger('TNM API Get Items')
-        log.info('Get items from TNM API with query: {}'.format(params))
+        log.info('Get items from TNM API with query: {}'.format(json.dumps(params, indent=4)))
 
-        response = requests.get(url, headers=TNM.HEADERS, params=params)
+        def curl_str():
+            """A little helper script to printout the curl command to replicate the request with all the params
+            """
+            encoded_params = urllib.parse.urlencode(params)
+            full_url = f"{url}?{encoded_params}"
+            cmd = 'curl --request GET --url "{}" --header "accept: application/json"'.format(full_url)
+            return '\n\nCurl command: {}\n'.format(cmd)
+
+        response = requests.get(url, headers=TNM.HEADERS, params=params, timeout=60)
+
+        log.debug('Response code: {}'.format(response.status_code))
+
+        if 'errorMessage' in response.text:
+            log.error(curl_str())
+            raise Exception('Failed to get items from TNM API with error message: {}'.format(response.text))
 
         if response.status_code == 200:
             try:
-                return response.json()
+                response = response.json()
+                log.debug(curl_str())
+                return response
             except json.JSONDecodeError as e:
+                log.error(curl_str())
                 log.error('Failed to decode JSON response: {}'.format(e))
                 log.info('Response text: {}'.format(response.text))
+                raise Exception('Failed to get items from TNM API')
 
         else:
-            log.error('Failed to get items from TNM API with status code: {}'.format(response.status_code))
+            log.error(curl_str())
             log.info('Response text: {}'.format(response.text))
+            raise Exception('Failed to get items from TNM API with status code: {}'.format(response.status_code))
