ae15ecce968f8f9153c51adbda0b006d412f2d79
Jordan Gilbert <jtgilbert89@gmail.com>
2024-09-10 21:03:59 +0000

    dam densities on dgos and igos

--- packages/brat/beaver_sign/beaver_sign.py
+++ packages/brat/beaver_sign/beaver_sign.py
@@ -6,6 +6,12 @@ from osgeo import ogr
 
 from rscommons import initGDALOGRErrors, ModelConfig, Logger, RSLayer, RSProject, RSMeta, RSMetaTypes, get_shp_or_gpkg, GeopackageLayer, dotenv
 from rscommons.augment_lyr_meta import augment_layermeta, add_layer_descriptions
+from rscommons.vector_ops import copy_feature_class
+from rscommons.database import SQLiteCon
+from rscommons.moving_window import moving_window_dgo_ids
+
+from beaver_sign.utils.dam_counts_to_dgos import dam_counts_to_dgos
+from beaver_sign.utils.riverscape_counts import riverscapes_dam_counts
 
 from beaver_sign.__version__ import __version__
 
@@ -17,13 +23,15 @@ LYR_DESCRIPTIONS_JSON = os.path.join(os.path.dirname(__file__), 'lyr_description
 
 LayerTypes = {
     'BEAVER_ACTIVITY': RSLayer('Beaver Activity', 'BeaverActivity', 'Geopackage', 'beaver_activity.gpkg', {
+        'DGOS': RSLayer('Discrete Geographic Objects', 'DGOS', 'Vector', 'dgos'),
+        'IGOS': RSLayer('Integrated Geographic Objects', 'IGOS', 'Vector', 'igos'),
         'DAMS': RSLayer('Dams', 'Dams', 'Vector', 'dams'),
         # 'SIGN': RSLayer('Sign', 'Sign', 'Vector', 'sign')
     })
 }
 
 
-def beaver_activity(huc, proj_boundary, beaver_dams, output_dir, beaver_sign=None):
+def beaver_activity(huc, proj_boundary, dgos, igos, beaver_dams, output_dir, beaver_sign=None):
 
     log = Logger('Beaver Activity')
 
@@ -84,6 +92,40 @@ def beaver_activity(huc, proj_boundary, beaver_dams, output_dir, beaver_sign=Non
                     new_ftr = None
         project.add_project_vector(out_gpkg_node, RSLayer('Sign', 'Sign', 'Vector', 'sign'))
 
+    # set window distances for different stream sizes
+    distancein = {
+        '0': 200,
+        '1': 400,
+        '2': 1200,
+        '3': 2000,
+        '4': 8000
+    }
+
+    log.info('Copying valley bottom DGOs')
+    dgos_out = os.path.join(output_gpkg_path, LayerTypes['BEAVER_ACTIVITY'].sub_layers['DGOS'].rel_path)
+    igos_out = os.path.join(output_gpkg_path, LayerTypes['BEAVER_ACTIVITY'].sub_layers['IGOS'].rel_path)
+    copy_feature_class(dgos, dgos_out, cfg.OUTPUT_EPSG)
+    copy_feature_class(igos, igos_out, cfg.OUTPUT_EPSG)
+
+    # list of level paths
+    with SQLiteCon(output_gpkg_path) as db:
+        db.conn.execute('CREATE INDEX ix_igo_levelpath on igos(level_path)')
+        db.conn.execute('CREATE INDEX ix_igo_segdist on igos(seg_distance)')
+        db.conn.execute('CREATE INDEX ix_igo_size on igos(stream_size)')
+        db.conn.execute('CREATE INDEX ix_dgo_levelpath on dgos(level_path)')
+        db.conn.execute('CREATE INDEX ix_dgo_segdist on dgos(seg_distance)')
+
+        db.conn.commit()
+
+        db.curs.execute('SELECT distinct level_path FROM dgos')
+        levelps = db.curs.fetchall()
+        levelpathsin = [lp['level_path'] for lp in levelps]
+
+    dam_counts_to_dgos(os.path.join(output_gpkg_path, LayerTypes['BEAVER_ACTIVITY'].sub_layers['DAMS'].rel_path), dgos_out)
+
+    windows = moving_window_dgo_ids(igos_out, dgos_out, levelpathsin, distancein)
+    riverscapes_dam_counts(output_gpkg_path, windows)
+
     add_layer_descriptions(project, LYR_DESCRIPTIONS_JSON, LayerTypes)
 
     log.info('Project created successfully')
@@ -94,6 +136,8 @@ def main():
     parser = argparse.ArgumentParser(description='Beaver Activity')
     parser.add_argument('huc', type=int, help='Hydrologic Unit Code')
     parser.add_argument('proj_boundary', type=str, help='Path to watershed boundary feature class')
+    parser.add_argument('dgos', type=str, help='Path to valley bottom DGOs')
+    parser.add_argument('igos', type=str, help='Path to integrated geographic objects')
     parser.add_argument('beaver_dams', type=str, help='Path to beaver dams shapefile')
     parser.add_argument('output_dir', type=str, help='Output directory')
     parser.add_argument('--beaver_sign', type=str, help='Path to beaver sign shapefile')
@@ -110,10 +154,10 @@ def main():
         if args.debug is True:
             from rscommons.debug import ThreadRun
             memfile = os.path.join(args.output_dir, 'mem_usage.log')
-            retcode, max_obj = ThreadRun(beaver_activity, memfile, args.huc, args.proj_boundary, args.beaver_dams, args.output_dir, args.beaver_sign)
+            retcode, max_obj = ThreadRun(beaver_activity, memfile, args.huc, args.proj_boundary, args.dgos, args.igos, args.beaver_dams, args.output_dir, args.beaver_sign)
             log.debug(f'Return code: {retcode} [Max process usage] {max_obj}')
         else:
-            beaver_activity(args.huc, args.proj_boundary, args.beaver_dams, args.output_dir, args.beaver_sign)
+            beaver_activity(args.huc, args.proj_boundary, args.dgos, args.igos, args.beaver_dams, args.output_dir, args.beaver_sign)
     except Exception as e:
         log.error(f'Error: {e}')
         traceback.print_exc(file=sys.stdout)
