e6c2dc9bc9e3ee31a35e86ba9cec36c568211c97
Jordan Gilbert <jtgilbert89@gmail.com>
2024-10-15 20:54:04 +0000

    test upload

---
+++ packages/brat/beaver_sign/utils/upload_projects.py
@@ -0,0 +1,23 @@
+import subprocess
+import os
+
+
+def upload_projects(directory: str, org: str, tags: str):
+    """iterate through the subdirectories in the given directory and upload them to the data exchange
+    under ownership of the specified organization
+
+    Args:
+        directory (str): path to the directory containing the projects to be uploaded
+        org (str): data exchange organization guid
+    """
+
+    subdirs = os.listdir(directory)
+    for subdir in subdirs:
+        subprocess.run(["rscli", "upload", subdir, "--org", org, "--tags", tags])
+
+
+in_dir = '/mnt/c/Users/jordang/Box/0_ET_AL/Projects/USA/Montana/2023_Montana_Statewide_Beaver_Dam_Census/work_data/Beaver_Activity_projects'
+in_org = '06439423-ee19-4040-9ebd-01c6e481a763'
+in_tags = 'MT_Dam_Census'
+
+upload_projects(in_dir, in_org, in_tags)
