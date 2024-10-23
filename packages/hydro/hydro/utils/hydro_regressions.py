7efca9570d2d313491253419a6a29da77e711f41
Jordan Gilbert <jtgilbert89@gmail.com>
2024-10-23 14:53:41 -0600

    testing updating equns in csv

--- packages/hydro/hydro/utils/hydro_regressions.py
+++ packages/hydro/hydro/utils/hydro_regressions.py
@@ -24,6 +24,11 @@ def generate_linear_regressions(data_dict, x_key, y_key):
     Returns:
     dict: Dictionary containing the better model's coefficients, intercept, and R^2 score.
     """
+    if len(data_dict[x_key]) != len(data_dict[y_key]):
+        raise ValueError('The independent and dependent variables must have the same number of values.')
+    if len(data_dict[x_key]) < 3:
+        raise ValueError('At least 3 data points are required to generate a linear regression model.')
+
     # Extract data from the dictionary
     X = np.array(data_dict[x_key]).reshape(-1, 1)
     y = np.array(data_dict[y_key])
@@ -35,8 +40,9 @@ def generate_linear_regressions(data_dict, x_key, y_key):
 
     # Create and train the log-transformed linear regression model
     X_log = np.log(X)
+    y_log = np.log(y)
     model_log = LinearRegression()
-    model_log.fit(X_log, y)
+    model_log.fit(X_log, y_log)
     r2_log = model_log.score(X_log, y)
 
     # Compare R^2 scores and return the better model
@@ -94,41 +100,43 @@ def generate_gage_data(db_path, huc, minimum_gages):
 
     out_data = {'DA': [], 'Qlow': [], 'Q2': []}
 
-    ct = 0
     # select hucs whose hucs = huc
-    curs.execute(f"SELECT * FROM gages WHERE HUC8 = '{huc}'")
+    curs.execute(f"SELECT da, min_discharge, peak_discharge FROM sites LEFT JOIN discharges on sites.site_no = discharges.site_no WHERE huc = '{huc}'")
     selection = curs.fetchall()
-    for gage in selection:
-        out_data['DA'].append(gage['DA'])
-        out_data['Qlow'].append(gage['Qlow'])
-        out_data['Q2'].append(gage['Q2'])
-        ct += 1
-
-    if ct >= minimum_gages:
+    if len(selection) >= minimum_gages:
+        for gage in selection:
+            if None not in gage:
+                out_data['DA'].append(gage[0])
+                out_data['Qlow'].append(gage[1])
+                out_data['Q2'].append(gage[2])
         level = 8
-        return out_data, ct, level
+        return out_data, len(selection), level
     else:
         # step up to huc6 and add to select huc[0:6] = huc[0:6]
-        curs.execute(f"SELECT * FROM gages WHERE SUBSTR(HUC6, 0, 6) = '{huc[0:6]}'")
+        curs.execute(f"SELECT da, min_discharge, peak_discharge FROM sites LEFT JOIN discharges on sites.site_no = discharges.site_no WHERE SUBSTR(huc, 0, 7) = '{huc[:6]}'")
         selection = curs.fetchall()
-        for gage in selection:
-            ct += 1
-
-    if ct >= minimum_gages:
-        level = 6
-        return out_data, ct, level
-    else:
-        # step up to huc4 and add to selection huc[0:4] = huc[0:4]
-        curs.execute(f"SELECT * FROM gages WHERE SUBSTR(HUC4, 0, 4) = '{huc[0:4]}'")
-        selection = curs.fetchall()
-        for gage in selection:
-            ct += 1
-
-    if ct >= minimum_gages:
-        level = 4
-        return out_data, ct, level
-    else:
-        raise ValueError(f"Insufficient gages for HUC {huc}")
+        if len(selection) >= minimum_gages:
+            for gage in selection:
+                if None not in gage:
+                    out_data['DA'].append(gage[0])
+                    out_data['Qlow'].append(gage[1])
+                    out_data['Q2'].append(gage[2])
+            level = 6
+            return out_data, len(selection), level
+        else:
+            # step up to huc4 and add to selection huc[0:4] = huc[0:4]
+            curs.execute(f"SELECT da, min_discharge, peak_discharge FROM sites LEFT JOIN discharges on sites.site_no = discharges.site_no WHERE SUBSTR(huc, 0, 5) = '{huc[:4]}'")
+            selection = curs.fetchall()
+            if len(selection) >= minimum_gages:
+                for gage in selection:
+                    if None not in gage:
+                        out_data['DA'].append(gage[0])
+                        out_data['Qlow'].append(gage[1])
+                        out_data['Q2'].append(gage[2])
+                level = 4
+                return out_data, len(selection), level
+            else:
+                raise ValueError(f"Insufficient gages for HUC {huc}")
 
 
 def update_watersheds_table(csv_path, db_path, operator):
@@ -142,23 +150,43 @@ def update_watersheds_table(csv_path, db_path, operator):
         reader = csv.DictReader(file)
         huc8s_qlow = [row['WatershedID'] for row in reader if row['Qlow'] == '']
         log.info(f"Updating {len(huc8s_qlow)} watersheds with missing Qlow values.")
+    with open(csv_path, mode='r', newline='') as file:
+        reader = csv.DictReader(file)
         huc8s_q2 = [row['WatershedID'] for row in reader if row['Q2'] == '']
         log.info(f"Updating {len(huc8s_q2)} watersheds with missing Q2 values.")
 
     for huc in huc8s_qlow:
+        if huc == '01040001':
+            print(huc)
         try:
             data, gage_ct, huc_level = generate_gage_data(db_path, huc, 5)
             qlow = generate_linear_regressions(data, 'DA', 'Qlow')
+            if huc in huc8s_q2:
+                q2 = generate_linear_regressions(data, 'DA', 'Q2')
+                huc8s_q2.remove(huc)
+            else:
+                q2_r2 = None
+
             if qlow['model_type'] == 'regular':
-                qlow_eqn = f"{qlow['intercept']} + {qlow['coefficients'][0]} * DRNAREA"
+                qlow_eqn = f"{qlow['coefficients'][0]} * DRNAREA"
                 qlow_r2 = qlow['r2_score']
             else:
                 qlow_eqn = f"{10 ** qlow['intercept']} * DRNAREA ** {qlow['coefficients'][0]}"
                 qlow_r2 = qlow['r2_score']
+            if q2['model_type'] == 'regular':
+                q2_eqn = f"{q2['coefficients'][0]} * DRNAREA"
+                q2_r2 = q2['r2_score']
+            else:
+                q2_eqn = f"{10 ** q2['intercept']} * DRNAREA ** {q2['coefficients'][0]}"
+                q2_r2 = q2['r2_score']
 
-            metadata[huc] = {'Operator': operator, 'DateCreated': datetime.datetime.now().isoformat(), 'NumGages': gage_ct, 'HucLevel': huc_level, 'QLowR2': qlow_r2}
+            
+
+            metadata[huc] = {'Operator': operator, 'DateCreated': datetime.datetime.now().isoformat(), 'NumGages': gage_ct, 'HucLevel': huc_level, 'QLowR2': qlow_r2, 'Q2R2': q2_r2}
 
             update_csv_rows(csv_path, 'WatershedID', huc, 'Qlow', qlow_eqn)  # is it going to be slow doing this one by one?
+            update_csv_rows(csv_path, 'WatershedID', huc, 'Q2', q2_eqn)
+            update_csv_rows(csv_path, 'WatershedID', huc, 'Metadata', json.dumps(metadata[huc]))
 
         except ValueError as e:
             log.error(e)
@@ -176,7 +204,7 @@ def update_watersheds_table(csv_path, db_path, operator):
 
             update_csv_rows(csv_path, 'WatershedID', huc, 'Q2', q2_eqn)
             if huc in metadata.keys():
-                metadata[huc].update({'Q2R2': q2_r2})
+                metadata[huc]['Q2R2'] = q2_r2
             else:
                 metadata[huc] = {'Operator': operator, 'DateCreated': datetime.datetime.now().isoformat(), 'NumGages': gage_ct, 'HucLevel': huc_level, 'Q2R2': q2_r2}
 
@@ -209,3 +237,7 @@ def main():
         sys.exit(1)
 
     sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
