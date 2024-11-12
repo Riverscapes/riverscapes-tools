516f2e42eda1df000c3adf7fcdbc2c8d81bff92c
Matt Reimer <matt@northarrowresearch.com>
2024-11-12 11:54:53 -0800

    version number bumps. Shapely still not installing

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,24 +5,28 @@ set -eu
 python3 --version
 python3 -m venv .venv
 # Make sure pip is at a good version
-.venv/bin/python -m pip install --upgrade pip
+.venv/bin/python3 -m ensurepip --upgrade
+.venv/bin/pip3 install --upgrade pip
+.venv/bin/pip3 install --upgrade setuptools
+
 # Cython and numpy need to go in before an explicit GDAL install
-.venv/bin/pip --timeout=120 install \
-  Cython==3.0.8
+.venv/bin/pip3 --timeout=120 install \
+  Cython==3.0.8 \
+  setuptools
 
 # Need numpy before GDAL
-.venv/bin/pip install GDAL==$(gdal-config --version)
+.venv/bin/pip3 install GDAL==$(gdal-config --version)
 
 # Now install everything else
-.venv/bin/pip --timeout=120 install -r requirements.txt
+.venv/bin/pip3 --timeout=120 install -r requirements.txt
 
 # Install our packages as being editable
-.venv/bin/pip install -e ./lib/commons
-.venv/bin/pip install -e ./packages/rscontext
-.venv/bin/pip install -e ./packages/vbet
-.venv/bin/pip install -e ./packages/brat
-.venv/bin/pip install -e ./packages/rme
-.venv/bin/pip install -e ./packages/hand
-.venv/bin/pip install -e ./packages/rcat
-.venv/bin/pip install -e ./packages/confinement
-.venv/bin/pip install -e ./packages/hydro
+.venv/bin/pip3 install -e ./lib/commons
+.venv/bin/pip3 install -e ./packages/rscontext
+.venv/bin/pip3 install -e ./packages/vbet
+.venv/bin/pip3 install -e ./packages/brat
+.venv/bin/pip3 install -e ./packages/rme
+.venv/bin/pip3 install -e ./packages/hand
+.venv/bin/pip3 install -e ./packages/rcat
+.venv/bin/pip3 install -e ./packages/confinement
+.venv/bin/pip3 install -e ./packages/hydro
