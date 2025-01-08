be329b0121c131745965387cf789c15460b8a67a
Jordan Gilbert <jtgilbert89@gmail.com>
2025-01-08 21:50:51 +0000

    beaver activity from qris project

--- packages/brat/beaver_sign/beaver_sign.py
+++ packages/brat/beaver_sign/beaver_sign.py
@@ -13,6 +13,7 @@ from rscommons.project_bounds import generate_project_extents_from_layer
 
 from beaver_sign.utils.dam_counts_to_dgos import dam_counts_to_dgos
 from beaver_sign.utils.riverscape_counts import riverscapes_dam_counts
+from beaver_sign.utils.census_info import census_info
 
 from beaver_sign.__version__ import __version__
 
@@ -22,118 +23,135 @@ cfg = ModelConfig('https://xml.riverscapes.net/Projects/XSD/V2/RiverscapesProjec
 
 LYR_DESCRIPTIONS_JSON = os.path.join(os.path.dirname(__file__), 'lyr_descriptions.json')
 
-LayerTypes = {
-    'BEAVER_ACTIVITY': RSLayer('Beaver Activity', 'BeaverActivity', 'Geopackage', 'beaver_activity.gpkg', {
-        'DGOS': RSLayer('Discrete Geographic Objects', 'DGOS', 'Vector', 'dgos'),
-        'IGOS': RSLayer('Integrated Geographic Objects', 'IGOS', 'Vector', 'igos'),
-        'DAMS': RSLayer('Dams', 'Dams', 'Vector', 'dams'),
-        # 'SIGN': RSLayer('Sign', 'Sign', 'Vector', 'sign')
-    })
-}
+# LayerTypes = {
+#     'BEAVER_ACTIVITY': RSLayer('Beaver Activity', 'BeaverActivity', 'Geopackage', 'beaver_activity.gpkg', {
+#         # 'DGOS': RSLayer('Discrete Geographic Objects', 'DGOS', 'Vector', 'dgos'),
+#         # 'IGOS': RSLayer('Integrated Geographic Objects', 'IGOS', 'Vector', 'igos'),
+#         # 'DAMS': RSLayer('Dams', 'Dams', 'Vector', 'dams'),
+#         # 'SIGN': RSLayer('Sign', 'Sign', 'Vector', 'sign')
+#     })
+# }
 
 
-def beaver_activity(huc, proj_boundary, dgos, igos, beaver_dams, output_dir, beaver_sign=None):
+def beaver_activity(huc, proj_boundary, dgos, igos, qris_path, output_dir, beaver_sign=None):
 
     log = Logger('Beaver Activity')
 
-    augment_layermeta('beaver_activity', LYR_DESCRIPTIONS_JSON, LayerTypes)
+    # augment_layermeta('beaver_activity', LYR_DESCRIPTIONS_JSON, LayerTypes)
 
     project_name = f'Beaver Activity for HUC {huc}'
     project = RSProject(cfg, output_dir)
     project.create(project_name, 'beaver_activity', [
         RSMeta('Model Documentation', 'https://tools.riverscapes.net/beaver_activity', RSMetaTypes.URL, locked=True),
         RSMeta('HUC', str(huc), RSMetaTypes.HIDDEN, locked=True),
-        RSMeta('Hydrologic Unit Code', str(huc), locked=True)
+        RSMeta('Hydrologic Unit Code', str(huc), locked=True),
+        RSMeta('Small Search Window', '200', RSMetaTypes.INT, locked=True),
+        RSMeta('Medium Search Window', '400', RSMetaTypes.INT, locked=True),
+        RSMeta('Large Search Window', '600', RSMetaTypes.INT, locked=True),
     ])
 
-    _realization, proj_nodes = project.add_realization(project_name, 'REALIZATION1', cfg.version, data_nodes=['Outputs'])
-
-    output_gpkg_path = os.path.join(output_dir, LayerTypes['BEAVER_ACTIVITY'].rel_path)
+    # find the number of different dam feature classes and their descriptions
+    census = census_info(qris_path)
 
     log.info('Setting up output feature classes')
-    with GeopackageLayer(output_gpkg_path, LayerTypes['BEAVER_ACTIVITY'].sub_layers['DAMS'].rel_path, delete_dataset=True) as out_lyr, \
-            get_shp_or_gpkg(beaver_dams) as dams, get_shp_or_gpkg(proj_boundary) as boundary:
-        out_lyr.create_layer(ogr.wkbMultiPoint, epsg=cfg.OUTPUT_EPSG, fields={
-            'dam_cer': ogr.OFTString,
-            'dam_type': ogr.OFTString,
-            'type_cer': ogr.OFTString
-        })
-        boundary_ftr = boundary.ogr_layer.GetNextFeature()
-        bbox = boundary_ftr.GetGeometryRef().GetEnvelope()
-        for ftr, *_ in dams.iterate_features(clip_rect=bbox):
-            if ftr.GetGeometryRef().Intersects(boundary_ftr.GetGeometryRef()):
-                dam_cer = ftr.GetField('dam_cer')  # are these always the same or should it be a list param
-                dam_type = ftr.GetField('type')
-                type_cer = ftr.GetField('type_cer')
-                new_ftr = ogr.Feature(out_lyr.ogr_layer.GetLayerDefn())
-                new_ftr.SetGeometry(ftr.GetGeometryRef())
-                new_ftr.SetField('dam_cer', dam_cer)
-                new_ftr.SetField('dam_type', dam_type)
-                new_ftr.SetField('type_cer', type_cer)
-                out_lyr.ogr_layer.CreateFeature(new_ftr)
-                new_ftr = None
+    # realization_nodes = {}
+    for i in range(len(census)):
+        r_num = i + 1
+        _realization, proj_nodes = project.add_realization(list(census.values())[i][1], f'REALIZATION{r_num}', cfg.version, data_nodes=['Outputs'], meta=[
+            RSMeta('Description', list(census.values())[i][0], locked=True),
+        ])
+        # realization_nodes[i] = proj_nodes
+        beaver_dams = os.path.join(qris_path, f'qris.gpkg/vw_beaver_dam_{r_num}')
 
-    out_gpkg_node, *_ = project.add_project_geopackage(proj_nodes['Outputs'], LayerTypes['BEAVER_ACTIVITY'])
+        rz_layers = {
+            'BEAVER_ACTIVITY': RSLayer('Beaver Activity', 'BeaverActivity', 'Geopackage', f'beaver_activity_{r_num}.gpkg', {
+                'DGOS': RSLayer('Discrete Geographic Objects', 'DGOS', 'Vector', 'beaver_activity_dgos'),
+                'IGOS': RSLayer('Integrated Geographic Objects', 'IGOS', 'Vector', 'beaver_activity_igos'),
+                'DAMS': RSLayer('Dams', 'Dams', 'Vector', 'dams'),
+                # 'SIGN': RSLayer('Sign', 'Sign', 'Vector', 'sign')
+            })
+        }
+        output_gpkg_path = os.path.join(output_dir, rz_layers['BEAVER_ACTIVITY'].rel_path)
 
-    if beaver_sign:
-        with GeopackageLayer(output_gpkg_path, LayerTypes['BEAVER_ACTIVITY'].sub_layers['SIGN'].rel_path, delete_dataset=True) as out_lyr, \
-                get_shp_or_gpkg(beaver_sign) as sign, get_shp_or_gpkg(proj_boundary) as boundary:
+        with GeopackageLayer(output_gpkg_path, rz_layers['BEAVER_ACTIVITY'].sub_layers['DAMS'].rel_path, write=True) as out_lyr, \
+                GeopackageLayer(beaver_dams) as dams, get_shp_or_gpkg(proj_boundary) as boundary:
             out_lyr.create_layer(ogr.wkbMultiPoint, epsg=cfg.OUTPUT_EPSG, fields={
-                'type': ogr.OFTString
+                'dam_cer': ogr.OFTString,
+                'dam_type': ogr.OFTString,
+                'type_cer': ogr.OFTString
             })
             boundary_ftr = boundary.ogr_layer.GetNextFeature()
             bbox = boundary_ftr.GetGeometryRef().GetEnvelope()
-            for ftr, *_ in sign.iterate_features(clip_rect=bbox):
+            for ftr, *_ in dams.iterate_features(clip_rect=bbox):
                 if ftr.GetGeometryRef().Intersects(boundary_ftr.GetGeometryRef()):
-                    dam_cer = ftr.GetField('sign_type')  # are these always the same or should it be a list param
+                    dam_cer = ftr.GetField('Dam CER')  # are these always the same or should it be a list param
+                    dam_type = ftr.GetField('Dam Type')
+                    type_cer = ftr.GetField('Type CER')
                     new_ftr = ogr.Feature(out_lyr.ogr_layer.GetLayerDefn())
                     new_ftr.SetGeometry(ftr.GetGeometryRef())
-                    new_ftr.SetField('type', dam_cer)
+                    new_ftr.SetField('dam_cer', dam_cer)
+                    new_ftr.SetField('dam_type', dam_type)
+                    new_ftr.SetField('type_cer', type_cer)
                     out_lyr.ogr_layer.CreateFeature(new_ftr)
                     new_ftr = None
-        project.add_project_vector(out_gpkg_node, RSLayer('Sign', 'Sign', 'Vector', 'sign'))
 
-    # set window distances for different stream sizes
-    distancein = {
-        '0': 200,
-        '1': 400,
-        '2': 1200,
-        '3': 2000,
-        '4': 8000
-    }
+            out_gpkg_node, *_ = project.add_project_geopackage(proj_nodes['Outputs'], rz_layers['BEAVER_ACTIVITY'])
+
+        if beaver_sign:
+            with GeopackageLayer(output_gpkg_path, rz_layers['BEAVER_ACTIVITY'].sub_layers['SIGN'].rel_path, delete_dataset=True) as out_lyr, \
+                    get_shp_or_gpkg(beaver_sign) as sign, get_shp_or_gpkg(proj_boundary) as boundary:
+                out_lyr.create_layer(ogr.wkbMultiPoint, epsg=cfg.OUTPUT_EPSG, fields={
+                    'type': ogr.OFTString
+                })
+                boundary_ftr = boundary.ogr_layer.GetNextFeature()
+                bbox = boundary_ftr.GetGeometryRef().GetEnvelope()
+                for ftr, *_ in sign.iterate_features(clip_rect=bbox):
+                    if ftr.GetGeometryRef().Intersects(boundary_ftr.GetGeometryRef()):
+                        sign = ftr.GetField('sign_type')  # are these always the same or should it be a list param
+                        new_ftr = ogr.Feature(out_lyr.ogr_layer.GetLayerDefn())
+                        new_ftr.SetGeometry(ftr.GetGeometryRef())
+                        new_ftr.SetField('type', sign)
+                        out_lyr.ogr_layer.CreateFeature(new_ftr)
+                        new_ftr = None
+            project.add_project_vector(out_gpkg_node, RSLayer('Sign', 'Sign', 'Vector', 'sign'))
 
     log.info('Copying valley bottom DGOs')
-    dgos_out = os.path.join(output_gpkg_path, LayerTypes['BEAVER_ACTIVITY'].sub_layers['DGOS'].rel_path)
-    igos_out = os.path.join(output_gpkg_path, LayerTypes['BEAVER_ACTIVITY'].sub_layers['IGOS'].rel_path)
-    copy_feature_class(dgos, dgos_out, cfg.OUTPUT_EPSG)
-    copy_feature_class(igos, igos_out, cfg.OUTPUT_EPSG)
+    windows = None
+    for i in range(len(census)):
+        output_gpkg_path = os.path.join(output_dir, f'beaver_activity_{i+1}.gpkg')
+        dgos_out = os.path.join(output_gpkg_path, 'beaver_activity_dgos')
+        igos_out = os.path.join(output_gpkg_path, 'beaver_activity_igos')
+        copy_feature_class(dgos, dgos_out, cfg.OUTPUT_EPSG)
+        copy_feature_class(igos, igos_out, cfg.OUTPUT_EPSG)
 
-    # list of level paths
-    with SQLiteCon(output_gpkg_path) as db:
-        db.conn.execute('CREATE INDEX ix_igo_levelpath on igos(level_path)')
-        db.conn.execute('CREATE INDEX ix_igo_segdist on igos(seg_distance)')
-        db.conn.execute('CREATE INDEX ix_igo_size on igos(stream_size)')
-        db.conn.execute('CREATE INDEX ix_dgo_levelpath on dgos(level_path)')
-        db.conn.execute('CREATE INDEX ix_dgo_segdist on dgos(seg_distance)')
+        # list of level paths
+        with SQLiteCon(output_gpkg_path) as db:
+            db.conn.execute('CREATE INDEX ix_igo_levelpath on beaver_activity_igos(level_path)')
+            db.conn.execute('CREATE INDEX ix_igo_segdist on beaver_activity_igos(seg_distance)')
+            db.conn.execute('CREATE INDEX ix_igo_size on beaver_activity_igos(stream_size)')
+            db.conn.execute('CREATE INDEX ix_dgo_levelpath on beaver_activity_dgos(level_path)')
+            db.conn.execute('CREATE INDEX ix_dgo_segdist on beaver_activity_dgos(seg_distance)')
 
-        db.conn.commit()
+            db.conn.commit()
 
-        db.curs.execute('SELECT distinct level_path FROM dgos')
-        levelps = db.curs.fetchall()
-        levelpathsin = [lp['level_path'] for lp in levelps]
+            db.curs.execute('SELECT distinct level_path FROM beaver_activity_dgos')
+            levelps = db.curs.fetchall()
+            levelpathsin = [lp['level_path'] for lp in levelps]
 
-    dam_counts_to_dgos(os.path.join(output_gpkg_path, LayerTypes['BEAVER_ACTIVITY'].sub_layers['DAMS'].rel_path), dgos_out)
+        if windows is None:
+            windows = moving_window_by_intersection(igos_out, dgos_out, levelpathsin)
 
-    # windows = moving_window_dgo_ids(igos_out, dgos_out, levelpathsin, distancein)
-    windows = moving_window_by_intersection(igos_out, dgos_out, levelpathsin)
-    riverscapes_dam_counts(output_gpkg_path, windows)
+        dams_in = os.path.join(output_gpkg_path, 'dams')
+        dam_counts_to_dgos(dams_in, dgos_out)
+
+        riverscapes_dam_counts(output_gpkg_path, windows)
 
     # add project extents
     extents_path = os.path.join(os.path.dirname(output_gpkg_path), 'project_bounds.geojson')
     extents = generate_project_extents_from_layer(proj_boundary, extents_path)
     project.add_project_extent(extents_path, extents['CENTROID'], extents['BBOX'])
 
-    add_layer_descriptions(project, LYR_DESCRIPTIONS_JSON, LayerTypes)
+    # add_layer_descriptions(project, LYR_DESCRIPTIONS_JSON, LayerTypes)
 
     log.info('Project created successfully')
 
@@ -145,7 +163,7 @@ def main():
     parser.add_argument('proj_boundary', type=str, help='Path to watershed boundary feature class')
     parser.add_argument('dgos', type=str, help='Path to valley bottom DGOs')
     parser.add_argument('igos', type=str, help='Path to integrated geographic objects')
-    parser.add_argument('beaver_dams', type=str, help='Path to beaver dams shapefile')
+    parser.add_argument('qris_path', type=str, help='Path to QRIS riverscapes project that contains dam census')
     parser.add_argument('output_dir', type=str, help='Output directory')
     parser.add_argument('--beaver_sign', type=str, help='Path to beaver sign shapefile')
     parser.add_argument('--verbose', help='(optional) a little extra logging', action='store_true', default=False)
@@ -161,10 +179,10 @@ def main():
         if args.debug is True:
             from rscommons.debug import ThreadRun
             memfile = os.path.join(args.output_dir, 'mem_usage.log')
-            retcode, max_obj = ThreadRun(beaver_activity, memfile, args.huc, args.proj_boundary, args.dgos, args.igos, args.beaver_dams, args.output_dir, args.beaver_sign)
+            retcode, max_obj = ThreadRun(beaver_activity, memfile, args.huc, args.proj_boundary, args.dgos, args.igos, args.qris_path, args.output_dir, args.beaver_sign)
             log.debug(f'Return code: {retcode} [Max process usage] {max_obj}')
         else:
-            beaver_activity(args.huc, args.proj_boundary, args.dgos, args.igos, args.beaver_dams, args.output_dir, args.beaver_sign)
+            beaver_activity(args.huc, args.proj_boundary, args.dgos, args.igos, args.qris_path, args.output_dir, args.beaver_sign)
     except Exception as e:
         log.error(f'Error: {e}')
         traceback.print_exc(file=sys.stdout)
--- packages/brat/beaver_sign/utils/riverscape_counts.py
+++ packages/brat/beaver_sign/utils/riverscape_counts.py
@@ -9,8 +9,8 @@ def riverscapes_dam_counts(gpkg_path: str, windows: dict):
     log = Logger('Moving Window Beaver Activity')
     log.info('Attributing IGOs with dam count attributes')
 
-    dgo = os.path.join(gpkg_path, 'dgos')
-    igo = os.path.join(gpkg_path, 'igos')
+    dgo = os.path.join(gpkg_path, 'beaver_activity_dgos')
+    igo = os.path.join(gpkg_path, 'beaver_activity_igos')
 
     out_data = {}
 
@@ -25,14 +25,14 @@ def riverscapes_dam_counts(gpkg_path: str, windows: dict):
             for dgo_ftr, *_ in dgo_lyr.iterate_features(attribute_filter=sql):
                 if dgo_ftr.GetField('centerline_length') is None or dgo_ftr.GetField('centerline_length') == 0:
                     continue
-                dam_ct += dgo_ftr.GetField('dam_ct')
+                dam_ct += dgo_ftr.GetField(f'dam_ct')
                 cl_len += dgo_ftr.GetField('centerline_length')
             if dam_ct > 0:
                 out_data[igo_ftr.GetFID()] = [dam_ct, cl_len]
 
     with GeopackageLayer(igo, write=True) as igo_lyr:
-        igo_lyr.ogr_layer.CreateField(ogr.FieldDefn('dam_ct', ogr.OFTInteger))
-        igo_lyr.ogr_layer.CreateField(ogr.FieldDefn('dam_density', ogr.OFTReal))
+        igo_lyr.ogr_layer.CreateField(ogr.FieldDefn(f'dam_ct', ogr.OFTInteger))
+        igo_lyr.ogr_layer.CreateField(ogr.FieldDefn(f'dam_density', ogr.OFTReal))
 
     ct = 0
     progbar = ProgressBar(len(out_data), text='Adding dam counts to IGOs')
@@ -40,9 +40,9 @@ def riverscapes_dam_counts(gpkg_path: str, windows: dict):
     for fid, dam_ct in out_data.items():
         ct += 1
         progbar.update(ct)
-        conn.ExecuteSQL(f'UPDATE igos SET dam_ct = {dam_ct[0]}, dam_density = {dam_ct[0] / (dam_ct[1] / 1000)} WHERE fid = {fid}')
+        conn.ExecuteSQL(f'UPDATE beaver_activity_igos SET dam_ct = {dam_ct[0]}, dam_density = {dam_ct[0] / (dam_ct[1] / 1000)} WHERE fid = {fid}')
 
-    conn.ExecuteSQL('UPDATE igos SET dam_ct = 0, dam_density = 0 WHERE dam_ct IS NULL')
+    conn.ExecuteSQL('UPDATE beaver_activity_igos SET dam_ct = 0, dam_density = 0 WHERE dam_ct IS NULL')
 
     # for ftr, *_ in igo_lyr.iterate_features('Attributing IGOs'):
     #     dam_ct, cl_len = out_data[ftr.GetFID()]
