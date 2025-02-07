a3b63a5cd6e1ff83fbfc1af79f5d93310163d66a
Jordan Gilbert <jtgilbert89@gmail.com>
2025-02-07 21:28:41 +0000

    different wats id method

--- packages/rme/rme/metric_engine.py
+++ packages/rme/rme/metric_engine.py
@@ -966,6 +966,10 @@ def metric_engine(huc: int, in_flowlines: Path, in_vaa_table: Path, in_counties:
                         bratopp = curs.fetchone()[0]
                     metrics_output[metric['metric_id']] = str(bratopp)
 
+                if 'WATERSHED' in metrics:
+                    metric = metrics['WATERSHED']
+                    metrics_output[metric['metric_id']] = str(huc)
+
                 # Write to Metrics
                 if len(metrics_output) > 0:
                     lp_metrics[dgo_id] = metrics_output
@@ -1148,6 +1152,10 @@ def metric_engine(huc: int, in_flowlines: Path, in_vaa_table: Path, in_counties:
                     if agency is not None:
                         curs.execute("""INSERT INTO igo_metric_values (igo_id, metric_id, metric_value) VALUES (?, ?, ?)""", (
                             igo_id, metrics['AGENCY']['metric_id'], str(agency)))
+
+                if 'WATERSHED' in metrics:
+                    curs.execute("""INSERT INTO igo_metric_values (igo_id, metric_id, metric_value) VALUES (?, ?, ?)""", (
+                        igo_id, metrics['WATERSHED']['metric_id'], str(huc)))
 
             if 'STRMGRAD' in metrics:
                 curs.execute(
@@ -1597,7 +1605,7 @@ def metric_engine(huc: int, in_flowlines: Path, in_vaa_table: Path, in_counties:
         sql2 = f"""CREATE VIEW igo_num_metrics (fid, {num_metric_names_sql}) AS SELECT M.igo_id, {metric_values_sql} FROM igo_metric_values M GROUP BY M.igo_id;"""
         curs.execute(sql2)
 
-        curs.execute(f"""CREATE VIEW dgo_text_metrics(fid, {text_metric_names_sql}) AS SELECT dgo.fid, o.ownership, s.us_state, c.county, e.ecoregion3, f.ecoregion4, n.name, br.bratrisk, bo.bratopp, {str(huc)} FROM dgos dgo LEFT JOIN
+        curs.execute(f"""CREATE VIEW dgo_text_metrics(fid, {text_metric_names_sql}) AS SELECT dgo.fid, o.ownership, s.us_state, c.county, e.ecoregion3, f.ecoregion4, n.name, br.bratrisk, bo.bratopp, w.watershed FROM dgos dgo LEFT JOIN
                      (SELECT dgo_id, metric_value AS ownership FROM dgo_metric_values WHERE metric_id={metrics['AGENCY']['metric_id']}) o ON o.dgo_id=dgo.fid LEFT JOIN
                      (SELECT dgo_id, metric_value AS us_state FROM dgo_metric_values WHERE metric_id={metrics['STATE']['metric_id']}) s ON s.dgo_id=dgo.fid LEFT JOIN
                      (SELECT dgo_id, metric_value AS county FROM dgo_metric_values WHERE metric_id={metrics['COUNTY']['metric_id']}) c ON c.dgo_id=dgo.fid LEFT JOIN
@@ -1605,9 +1613,10 @@ def metric_engine(huc: int, in_flowlines: Path, in_vaa_table: Path, in_counties:
                      (SELECT dgo_id, metric_value AS ecoregion3 FROM dgo_metric_values WHERE metric_id={metrics['ECORGIII']['metric_id']}) e ON e.dgo_id=dgo.fid LEFT JOIN
                      (SELECT dgo_id, metric_value AS ecoregion4 FROM dgo_metric_values WHERE metric_id={metrics['ECORGIV']['metric_id']}) f ON f.dgo_id=dgo.fid LEFT JOIN
                      (SELECT dgo_id, metric_value AS bratrisk FROM dgo_metric_values WHERE metric_id={metrics['BRATRISK']['metric_id']}) br ON br.dgo_id=dgo.fid LEFT JOIN
-                     (SELECT dgo_id, metric_value AS bratopp FROM dgo_metric_values WHERE metric_id={metrics['BRATOPP']['metric_id']}) bo ON bo.dgo_id=dgo.fid
+                     (SELECT dgo_id, metric_value AS bratopp FROM dgo_metric_values WHERE metric_id={metrics['BRATOPP']['metric_id']}) bo ON bo.dgo_id=dgo.fid LEFT JOIN
+                     (SELECT dgo_id, metric_value AS watershed FROM dgo_metric_values WHERE metric_id={metrics['WATERSHED']['metric_id']}) w ON w.dgo_id=dgo.fid
                      """)
-        curs.execute(f"""CREATE VIEW igo_text_metrics(fid, {text_metric_names_sql}) AS SELECT igo.fid, o.ownership, s.us_state, c.county, e.ecoregion3, f.ecoregion4, n.name, br.bratrisk, bo.bratopp, {str(huc)} FROM igos igo LEFT JOIN
+        curs.execute(f"""CREATE VIEW igo_text_metrics(fid, {text_metric_names_sql}) AS SELECT igo.fid, o.ownership, s.us_state, c.county, e.ecoregion3, f.ecoregion4, n.name, br.bratrisk, bo.bratopp, w.watershed FROM igos igo LEFT JOIN
                      (SELECT igo_id, metric_value AS ownership FROM igo_metric_values WHERE metric_id={metrics['AGENCY']['metric_id']}) o ON o.igo_id=igo.fid LEFT JOIN
                      (SELECT igo_id, metric_value AS us_state FROM igo_metric_values WHERE metric_id={metrics['STATE']['metric_id']}) s ON s.igo_id=igo.fid LEFT JOIN
                      (SELECT igo_id, metric_value AS county FROM igo_metric_values WHERE metric_id={metrics['COUNTY']['metric_id']}) c ON c.igo_id=igo.fid LEFT JOIN
@@ -1615,8 +1624,10 @@ def metric_engine(huc: int, in_flowlines: Path, in_vaa_table: Path, in_counties:
                      (SELECT igo_id, metric_value AS ecoregion3 FROM igo_metric_values WHERE metric_id={metrics['ECORGIII']['metric_id']}) e ON e.igo_id=igo.fid LEFT JOIN
                      (SELECT igo_id, metric_value AS ecoregion4 FROM igo_metric_values WHERE metric_id={metrics['ECORGIV']['metric_id']}) f ON f.igo_id=igo.fid LEFT JOIN
                      (SELECT igo_id, metric_value AS bratrisk FROM igo_metric_values WHERE metric_id={metrics['BRATRISK']['metric_id']}) br ON br.igo_id=igo.fid LEFT JOIN
-                     (SELECT igo_id, metric_value AS bratopp FROM igo_metric_values WHERE metric_id={metrics['BRATOPP']['metric_id']}) bo ON bo.igo_id=igo.fid
+                     (SELECT igo_id, metric_value AS bratopp FROM igo_metric_values WHERE metric_id={metrics['BRATOPP']['metric_id']}) bo ON bo.igo_id=igo.fid LEFT JOIN
+                     (SELECT igo_id, metric_value AS watershed FROM igo_metric_values WHERE metric_id={metrics['WATERSHED']['metric_id']}) w ON w.igo_id=igo.fid
                      """)
+
         curs.execute(
             "CREATE VIEW dgo_metrics_pivot AS SELECT * FROM dgo_num_metrics JOIN dgo_text_metrics USING (fid);")
         curs.execute(
