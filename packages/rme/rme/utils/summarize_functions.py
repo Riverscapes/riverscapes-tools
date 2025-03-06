bf608d538eec5b08e68400c7c33861e0a17e4ab0
Jordan Gilbert <jtgilbert89@gmail.com>
2025-03-06 21:20:03 +0000

    new functions applied to existing dgo metrics

--- packages/rme/rme/utils/summarize_functions.py
+++ packages/rme/rme/utils/summarize_functions.py
@@ -1,3 +1,4 @@
+import sqlite3
 from rscommons import GeopackageLayer, Logger
 
 
@@ -33,5 +34,49 @@ def value_from_max_length(field_name, in_line_network, dgo_ftr):
     else:
         majority_attribute = str(
             max(attributes, key=attributes.get))
+
+    return majority_attribute
+
+
+def value_from_dgo(geopackage, dgoid, layer_name, field_name):
+    with sqlite3.connect(geopackage) as conn:
+        curs = conn.cursor()
+        curs.execute(
+            f"SELECT {field_name} FROM {layer_name} WHERE fid = {dgoid}")
+        value = curs.fetchone()[0]
+
+    return value
+
+
+def value_density_from_dgo(geopackage, dgoid, layer_name, field_name):
+    with sqlite3.connect(geopackage) as conn:
+        curs = conn.cursor()
+        curs.execute(
+            f"SELECT {field_name}, centerline_length FROM {layer_name} WHERE fid = {dgoid}")
+        val = curs.fetchone()
+        if val[0] is not None and val[1] is not None:
+            density = val[0] / \
+                val[1] if val[1] > 0.0 else None
+        else:
+            density = None
+
+    return density
+
+
+def value_from_dataset_area(field_name, dataset, dgo_ftr):
+    attributes = {}
+    with GeopackageLayer(dataset) as lyr:
+        for feat, *_ in lyr.iterate_features(clip_shape=dgo_ftr):
+            geom_county = feat.GetGeometryRef()
+            attribute = feat.GetField(field_name)
+            geom_section = dgo_ftr.Intersection(geom_county)
+            area = geom_section.GetArea()
+            attributes[attribute] = attributes.get(
+                attribute, 0) + area
+        lyr = None
+    if len(attributes) == 0:
+        majority_attribute = None
+    else:
+        majority_attribute = str(max(attributes, key=attributes.get))
 
     return majority_attribute
