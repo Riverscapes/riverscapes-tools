f5d3bbae700198c2bde850c023f7f42df8301e6c
Jordan Gilbert <jtgilbert89@gmail.com>
2025-03-07 23:31:17 +0000

    building out schema

--- packages/rme/rme/database/metrics_schema.sql
+++ packages/rme/rme/database/metrics_schema.sql
@@ -1,3 +1,15 @@
+CREATE TABLE metric_groups (
+    metric_group_id INTEGER PRIMARY KEY NOT NULL,
+    metric_group_name TEXT,
+    description TEXT
+);
+
+CREATE TABLE metric_calculations (
+    metric_calculation_id INTEGER PRIMARY KEY NOT NULL,
+    calculation_method TEXT,
+    description TEXT
+);
+
 CREATE TABLE metrics (
     metric_id INTEGER PRIMARY KEY NOT NULL,
     name TEXT UNIQUE NOT NULL,
@@ -12,22 +24,10 @@ CREATE TABLE metrics (
     metric_group_id INTEGER,
     metric_calculation_id INTEGER,
     is_active BOOLEAN,
-    docs_url TEXT
+    docs_url TEXT,
 
     CONSTRAINT fk_metric_group_id FOREIGN KEY (metric_group_id) REFERENCES metric_groups (metric_group_id),
     CONSTRAINT fk_metric_calculation_id FOREIGN KEY (metric_calculation_id) REFERENCES metric_calculations (metric_calculation_id)
-);
-
-CREATE TABLE metric_groups (
-    metric_group_id INTEGER PRIMARY KEY NOT NULL,
-    metric_group_name TEXT,
-    description TEXT
-);
-
-CREATE TABLE metric_calculations (
-    metric_calculation_id INTEGER PRIMARY KEY NOT NULL,
-    calculation_method TEXT,
-    description TEXT
 );
 
 CREATE TABLE VegetationTypes (
@@ -52,7 +52,7 @@ CREATE TABLE measurement_values (
     measurement_value REAL,
     metadata TEXT,
     qaqc_date TEXT,
-    PRIMARY KEY (dgo_id, measurement_id)
+    PRIMARY KEY (dgo_id, measurement_id),
     
     CONSTRAINT fk_measurement_values_dgo_id FOREIGN KEY (dgo_id) REFERENCES dgos (fid),
     CONSTRAINT fk_measurement_values_measurement_id FOREIGN KEY (measurement_id) REFERENCES measurements (measurement_id)
@@ -121,14 +121,27 @@ CREATE TABLE dgo_dataset_metrics (
     hist_prop_sparsely_vegetated REAL
 );
 
+CREATE TABLE dgo_rme_metrics (
+    DGOID INTEGER PRIMARY KEY NOT NULL,
+    prim_channel_gradient REAL,
+    valley_gradient REAL,
+    relative_flow_length REAL,
+    confluences INTEGER,
+    diffluences INTEGER,
+    trib_junctions REAL,
+    planform_sinuosity REAL
+);
+
 
 CREATE INDEX fx_measurement_values_measurement_id ON measurement_values (measurement_id);
 
-CREATE INDEX ix_dgo_metric_values_metric_id ON dgo_metric_values (metric_id);
-CREATE INDEX ix_igo_metric_values_metric_id ON igo_metric_values (metric_id);
+-- CREATE INDEX ix_dgo_metric_values_metric_id ON dgo_metric_values (metric_id);
+-- CREATE INDEX ix_igo_metric_values_metric_id ON igo_metric_values (metric_id);
 
-INSERT INTO gpkg_contents (table_name, data_type) VALUES ('dgo_metric_values', 'attributes');
+INSERT INTO gpkg_contents (table_name, data_type) VALUES ('metric_groups', 'attributes');
+INSERT INTO gpkg_contents (table_name, data_type) VALUES ('metric_calculations', 'attributes');
 INSERT INTO gpkg_contents (table_name, data_type) VALUES ('metrics', 'attributes');
-INSERT INTO gpkg_contents (table_name, data_type) VALUES ('igo_metric_values', 'attributes');
-INSERT INTO gpkg_contents (table_name, data_type) VALUES ('measurement_values', 'attributes');
+INSERT INTO gpkg_contents (table_name, data_type) VALUES ('VegetationTypes', 'attributes');
 INSERT INTO gpkg_contents (table_name, data_type) VALUES ('measurements', 'attributes');
+INSERT INTO gpkg_contents (table_name, data_type) VALUES ('measurement_values', 'attributes');
+INSERT INTO gpkg_contents (table_name, data_type) VALUES ('dgo_dataset_metrics', 'attributes');
