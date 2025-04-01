bc58a39ddd53771ba26e8a226bd954698e5129ee
Lorin <weblorin@users.noreply.github.com>
2025-04-01 01:36:22 +0000

    start the nz-to-it conversion for issue #1110

---
+++ packages/rscontext_it/rscontextit/calc_level_path.py
@@ -0,0 +1,129 @@
+import os
+import argparse
+import sqlite3
+from rscommons import Logger
+
+NEXT_REACH_QUERY = 'SELECT us.Shape_length, ds.HydroID FROM riverlines us LEFT JOIN riverlines ds on us.To_NODE = ds.FROM_NODE WHERE us.HydroID = ?'
+
+
+def calc_level_path(curs: sqlite3.Cursor, watershed_id: int, reset_first: bool) -> None:
+
+    log = Logger('Calc Level Path')
+    log.info(f'Calculating level path for watershed {watershed_id}')
+
+    curs.execute('SELECT Count(*) FROM riverlines WHERE WatershedHydroID = ?', [watershed_id])
+    row = curs.fetchone()
+    log.info(f'Found {row[0]} reaches in watershed {watershed_id}')
+
+    curs.execute('SELECT Count(*) FROM riverlines WHERE WatershedHydroID = ? AND level_path IS NULL', [watershed_id])
+    row = curs.fetchone()
+    log.info(f'Found {row[0]} reaches without level path in watershed {watershed_id}')
+
+    if reset_first is True:
+        log.info(f'Resetting level paths for watershed {watershed_id}')
+        curs.execute("UPDATE riverlines SET level_path = NULL WHERE WatershedHydroID = ?", [watershed_id])
+
+    curs.execute("SELECT HydroID FROM riverlines WHERE Headwater <> 0 and WatershedHydroID = ? and level_path IS NULL", [watershed_id])
+    level_path_lengths = {row[0]: 0.00 for row in curs.fetchall()}
+    log.info(f'Found {len(level_path_lengths)} headwaters in watershed {watershed_id}')
+
+    for hydro_id in level_path_lengths.keys():
+        level_path_lengths[hydro_id] = calculate_length(curs, hydro_id)
+
+    num_processed = 0
+    log.info('Assigning level paths to reaches...')
+    for hydro_id, _length in sorted(level_path_lengths.items(), key=lambda item: item[1], reverse=True):
+        # print(f"{key}: {value}")
+        num_processed += 1
+        new_level_path = float(watershed_id * 10**9 + num_processed)
+        num_reaches = assign_level_path(curs, hydro_id, new_level_path)
+        # log.debug(f'Assigned level path {new_level_path} to {num_reaches} reaches starting at HydroID {hydro_id}')
+
+    log.info(f'Assigned level paths to {num_processed} headwaters in watershed {watershed_id}')
+
+
+def get_triggers(curs: sqlite3.Cursor, table: str):
+    """Get the triggers for the watershed."""
+    curs.execute("SELECT * FROM sqlite_master WHERE type = 'trigger' AND tbl_name = ?", [table])
+    return curs.fetchall()
+
+
+def calculate_length(curs: sqlite3.Cursor, hydro_id: int):
+    """Calculate the cumulative length of a riverline from the headwater to the mouth."""
+
+    cum_length = 0
+    while hydro_id is not None:
+        # Get the length of the current, upstream, riverline as well as the HydroID of the downstream reach.
+        curs.execute(NEXT_REACH_QUERY, [hydro_id])
+        row = curs.fetchall()
+        if len(row) == 1:
+            cum_length += row[0][0]
+            hydro_id = row[0][1]
+        elif row is None or len(row) == 0:
+            return cum_length
+        else:
+            raise Exception(f"More than one downstream reach found for HydroID {hydro_id}")
+
+    return cum_length
+
+
+def assign_level_path(curs: sqlite3.Cursor, headwater_hydro_id, level_path: float) -> int:
+    """Assign a level path to each reach starting at the headwater down to the ocean."""
+
+    num_reaches = 0
+    hydro_id = headwater_hydro_id
+    while hydro_id is not None:
+        num_reaches += 1
+        curs.execute('UPDATE riverlines SET level_path = ? WHERE HydroID = ? AND level_path IS NULL', [level_path, hydro_id])
+        curs.execute(NEXT_REACH_QUERY, [hydro_id])
+        row = curs.fetchall()
+        if len(row) == 1:
+            hydro_id = row[0][1]
+        elif row is None or len(row) == 0:
+            # log.debug(f'Assigned path {level_path} to {num_reaches} reaches starting at HydroID {headwater_hydro_id}')
+            return num_reaches
+        else:
+            raise Exception(f"More than one downstream reach found for HydroID {hydro_id}")
+
+    return num_reaches
+
+
+def main():
+    """Calculate the level path for each reach in a single watershed."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('hydro_gpkg', type=str)
+    parser.add_argument('watershed_id', type=int)
+    parser.add_argument('reset_first', type=str)
+    args = parser.parse_args()
+
+    # Initiate the log file
+    log = Logger("RS Context")
+    log.setup(logPath=os.path.join(os.path.dirname(args.hydro_gpkg), "nz_calc_level_path.log"), verbose=args.verbose)
+    log.title(f'Calculate level path For NZ Watershed: {args.watershed_id}')
+
+    log.info(f'HUC: {args.watershed_id}')
+    log.info(f'Hydro GPKG: {args.hydro_gpkg}')
+    log.info(f'Reset First: {args.reset_first}')
+
+    with sqlite3.connect(args.hydro_gpkg) as conn:
+        curs = conn.cursor()
+        try:
+            triggers = get_triggers(curs, 'riverlines')
+
+            for trigger in triggers:
+                curs.execute(f"DROP TRIGGER {trigger[1]}")
+
+            calc_level_path(curs, args.watershed_id, args.reset_first.lower() == 'true')
+            conn.commit()
+            log.info('Calculation complete')
+
+            for trigger in triggers:
+                curs.execute(trigger[4])
+
+        except Exception as e:
+            conn.rollback()
+            raise e
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/rscontext_it/rscontextit/rs_context_it.py
@@ -0,0 +1,324 @@
+"""
+Name:       Riverscapes Context Italy
+
+Purpose:    Build a Riverscapes Context project for a single Italy watershed
+
+Setup:      1. Use a GitHub CodeSpace within the Riverscapes Tools repo to run this script.
+            2. Ensure you are using the RSContextIT Workspace.
+            2. Use rscli to download the Italy National Project (project id = '8455c796-9ac1-4eb4-993c-40ff400c6693') to /Workspace/data.
+            3. Use the Debug command and pick "RS Context - ITALY" to run this script.
+            4. When prompted, provide the Watershed ID for the watershed (HUC) you want to process.
+                This must correspond to a feature in the national hydrography watersheds feature class.
+            5. The script will process the hydrography and topography data for the specified watershed.
+            6. The output will be saved to the /Workspace/output folder.
+            7. If you want to keep the output, use rscli to upload the output project to the Riverscapes Data Exchange.
+
+Author:     Lorin Gaertner - based on Riverscapes Context NZ by Philip Bailey
+
+Date:       2025-03-31
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+from typing import Tuple, Dict
+import argparse
+import sqlite3
+import json
+import os
+import sys
+import traceback
+from osgeo import ogr
+
+from rscommons import Logger, ModelConfig, dotenv, initGDALOGRErrors
+from rscommons.classes.rs_project import RSLayer, RSProject, RSMeta, RSMetaTypes
+from rscommons.geographic_raster import gdal_dem_geographic
+from rscommons.project_bounds import generate_project_extents_from_geom
+from rscommons.raster_warp import raster_warp
+from rscommons.util import safe_makedirs, parse_metadata
+from rscommons.vector_ops import copy_feature_class
+from rscommons.classes.vector_classes import GeopackageLayer
+from rscommons.shapefile import get_transform_from_epsg
+from rscontextnz.__version__ import __version__
+from .calc_level_path import calc_level_path, get_triggers
+
+cfg = ModelConfig('https://xml.riverscapes.net/Projects/XSD/V2/RiverscapesProject.xsd', __version__)
+
+initGDALOGRErrors()
+
+LayerTypes = {
+    # key: (name, id, tag, relpath)
+    'DEM': RSLayer('8m DEM', 'DEM', 'Raster', 'topography/dem.tif'),
+    'HILLSHADE': RSLayer('DEM Hillshade', 'HILLSHADE', 'Raster', 'topography/dem_hillshade.tif'),
+    'SLOPE': RSLayer('Slope', 'SLOPE', 'Raster', 'topography/slope.tif'),
+    'HYDRO': RSLayer('Hydrography', 'HYDROGRAPHY', 'Geopackage', 'hydrography/hydrography.gpkg', {
+        'FlowLines': RSLayer('Flow Lines', 'riverlines', 'Vector', 'riverlines'),
+        'Watersheds': RSLayer('Watersheds', 'watersheds', 'Vector', 'watersheds'),
+        'Catchments': RSLayer('Catchments', 'catchments', 'Vector', 'catchments'),
+        'Junctions': RSLayer('Hydro Junctions', 'junctions', 'Vector', 'junctions'),
+        'Lakes': RSLayer('Lakes', 'lakes', 'Vector', 'lakes'),
+    }),
+}
+
+
+def rs_context_it(watershed_id: str, natl_hydro_gpkg: str, dem: str, output_folder: str, meta: Dict[str, str]) -> None:
+    """
+    Run the Riverscapes Context Tool for Italy for a single watershed.
+    This function processes hydrographic and topographic data for a specified watershed.
+
+    Parameters:
+    watershed_id (str): Watershed ID for the watershed to be processed.
+    natl_hydro_gpkg (str): Path to the national hydrography GeoPackage.
+    dem (str): Path to the DEM raster.
+    output_folder (str): Directory where the output files will be saved.
+    """
+
+    log = Logger('RS Context')
+
+    safe_makedirs(output_folder)
+
+    hydro_gpkg, ws_name, trans_geom, ws_boundary_path = process_hydrography(natl_hydro_gpkg, watershed_id, output_folder)
+    dem, slope, hillshade = process_topography(dem, output_folder, ws_boundary_path)
+
+    # Write a the project bounds as a GeoJSON file and return the centroid and bounding box
+    bounds_file = os.path.join(output_folder, 'project_bounds.geojson')
+    bounds_info = generate_project_extents_from_geom(trans_geom, bounds_file)
+
+    metrics_file = os.path.join(output_folder, 'rscontext_metrics.json')
+    calculate_metrics(hydro_gpkg, dem, slope, metrics_file)
+
+    # Build the Riverscapes Context project
+    project_name = f'Riverscapes Context for {ws_name}'
+    project = RSProject(cfg, output_folder)
+    project.create(project_name, 'RSContextNZ', [
+        RSMeta('Model Documentation', 'https://tools.riverscapes.net/rscontextnz', RSMetaTypes.URL, locked=True),
+        RSMeta('HUC', str(watershed_id), RSMetaTypes.HIDDEN, locked=True),
+        RSMeta('Hydrologic Unit Code', str(watershed_id), locked=True),
+        RSMeta('Watershed Name', ws_name, RSMetaTypes.HIDDEN, locked=True),
+    ])
+
+    project.add_project_extent(bounds_file, bounds_info['CENTROID'], bounds_info['BBOX'])
+    project.add_metadata([RSMeta(key, val, RSMetaTypes.HIDDEN, locked=True) for key, val in meta.items()])
+
+    realization = project.add_realization(project_name, 'REALIZATION1', cfg.version)
+    datasets = project.XMLBuilder.add_sub_element(realization, 'Datasets')
+
+    project.add_project_geopackage(datasets, LayerTypes['HYDRO'])
+    project.add_dataset(datasets, metrics_file, RSLayer('Metrics', 'Metrics', 'File', os.path.basename(metrics_file)), 'File')
+    project.add_dataset(datasets, dem, LayerTypes['DEM'], 'DEM')
+    project.add_dataset(datasets, slope, LayerTypes['SLOPE'], 'Raster')
+    project.add_dataset(datasets, hillshade, LayerTypes['HILLSHADE'], 'Raster')
+
+    log.info('Riverscapes Context processing complete')
+
+
+def process_hydrography(national_hydro_gpkg: str, watershed_id: str, output_folder: str) -> Tuple[str, str, ogr.Geometry]:
+    """
+    Process the hydrography data for the specified watershed.
+
+    This function processes the hydrography data for a given watershed by clipping the national hydrography 
+    feature classes to the watershed boundary and saving the results to an output GeoPackage.
+
+    Parameters:
+    hydro_gpkg (str): Path to the GeoPackage containing national hydrography feature classes.
+    watershed_id (str): Identifier for the watershed.
+    output_folder (str): Directory where the output files will be saved.
+
+    Returns:
+    Tuple[str, str, geom]: A tuple containing the path to the output GeoPackage, the name of the watershed,
+    the watershed boundary geometry
+    """
+
+    log = Logger('Hydrography')
+    log.info(f'Processing Hydrography for watershed {watershed_id}')
+
+    watershed_layer = 'sample_watershed_99'
+    # sample_watershed hardcode
+    input_watersheds = os.path.join('sample_watershed.gpkg', watershed_layer)
+    input_rivers = os.path.join(national_hydro_gpkg, 'hydro_net_l')
+
+    # Load the watershed boundary polygon (in original projection)
+    orig_ws_boundary, trans_geom = get_geometry(national_hydro_gpkg,  watershed_layer, f'HydroID={watershed_id}', cfg.OUTPUT_EPSG)
+
+    # Retrieve the watershed name
+    with sqlite3.connect(national_hydro_gpkg) as conn:
+        curs = conn.cursor()
+        # SQLite does not support parameterizing table or column names. These must be hardcoded or dynamically constructed in the query string.
+        query = f'SELECT RivName, island FROM {watershed_layer} WHERE HydroID = ?'
+        curs.execute(query, [watershed_id])
+        row = curs.fetchone()
+        watershed_name = row[0]
+
+    # Clip the national hydrography feature classes into the output GeoPackage
+    output_gpkg = os.path.join(output_folder, 'hydrography', 'hydrography.gpkg')
+    output_ws = os.path.join(output_gpkg, 'watersheds')
+    copy_feature_class(input_watersheds, output_ws, 2193, attribute_filter=f'"HydroID"=\'{watershed_id}\'', make_valid=True)
+    copy_feature_class(input_rivers, os.path.join(output_gpkg, 'riverlines'), cfg.OUTPUT_EPSG, clip_shape=orig_ws_boundary, make_valid=True)
+
+    with sqlite3.connect(output_gpkg) as conn:
+        curs = conn.cursor()
+
+        # Drop triggers so that we can update the feature class
+        log.info('Dropping triggers to allow for updating level path')
+        triggers = get_triggers(curs, 'riverlines')
+        for trigger in triggers:
+            curs.execute(f"DROP TRIGGER {trigger[1]}")
+
+        # Add the riverscape fields to the riverlines feature class
+        for field, data_type in [('level_path', 'REAL'), ('FCode', 'INTEGER'), ('TotDASqKM', 'REAL')]:
+            curs.execute(f'ALTER TABLE riverlines ADD COLUMN {field} {data_type}')
+
+        for field in ['level_path', 'HydroID', 'FCode', 'TO_NODE', 'FROM_NODE']:
+            curs.execute(f'CREATE INDEX idx_{field} ON riverlines({field})')
+
+        # Assign level paths to all reaches in the GeoPackage
+        calc_level_path(curs, watershed_id, True)
+
+        # Assign FCode. Apply the NHD artifical path code to any features that have an LID (presumed to be Lake ID)
+        curs.execute('UPDATE riverlines SET FCode = ? WHERE LID <> 0', [55800])
+        curs.execute('UPDATE riverlines SET FCode = ? WHERE LID = 0', [46006])
+
+        # Assign Drainage Area to the riverlines
+        curs.execute('UPDATE riverlines SET TotDASqKM = CUM_AREA / 1000000.0')
+
+        log.info('Recreating triggers')
+        for trigger in triggers:
+            curs.execute(trigger[4])
+
+        conn.commit()
+
+    log.info(f'Hydrography processed and saved to {output_gpkg}')
+
+    return output_gpkg, watershed_name, trans_geom, output_ws
+
+
+def get_geometry(gpkg: str, layer_name: str, where_clause: str, output_epsg: int) -> Tuple[ogr.Geometry]:
+    """
+    Retrieves the geometry for a feature in a specified GeoPackage layer, 
+    optionally transforming it to a target spatial reference system (SRS).
+
+    Args:
+        gpkg (str): The file path to the GeoPackage.
+        layer_name (str): The name of the layer within the GeoPackage to query.
+        where_clause (str): An SQL-like WHERE clause to filter the features.
+        output_epsg (int): The EPSG code of the target spatial reference system 
+            to which the geometry should be transformed.
+
+    Returns:
+        Tuple[ogr.Geometry]: A tuple containing the original geometry and the 
+        transformed geometry in the target SRS.
+
+    Raises:
+        ValueError: If no features or multiple features match the `where_clause`.
+    """
+    with GeopackageLayer(gpkg, layer_name) as in_layer:
+        output_srs, _transform = get_transform_from_epsg(in_layer.spatial_ref, output_epsg)
+
+        feature_count = 0
+        original_geom = None
+        transform_geom = None
+
+        for rme_feature, *_ in in_layer.iterate_features(attribute_filter=where_clause):
+            feature_count += 1
+            if feature_count > 1:
+                raise ValueError(f"Multiple features ({feature_count}) found for where_clause: '{where_clause}' in layer '{layer_name}'. Expected exactly one feature.")
+
+            rme_feature: ogr.Feature
+            original_geom: ogr.Geometry = rme_feature.GeometryetGeometryRef().Clone()
+            transform_geom = original_geom.Clone()
+            transform_geom.TransformTo(output_srs)
+
+        if feature_count == 0:
+            raise ValueError(f"No features found for where_clause: '{where_clause}' in layer '{layer_name}'.")
+
+        return original_geom, transform_geom
+
+
+def calculate_metrics(output_gpkg: str, dem_path: str, slope_path: str, output_file: str) -> dict:
+    """
+    Calculate some basic metrics
+    """
+    log = Logger('Metrics')
+    log.info('Calculating metrics for the watershed')
+
+    metrics = {}
+    with sqlite3.connect(output_gpkg) as conn:
+        curs = conn.cursor()
+
+        curs.execute('SELECT RivName, island, CUM_AREA FROM watersheds')
+        row = curs.fetchone()
+        metrics['watershedName'] = row[0]
+        metrics['island'] = 'north' if row[1] == 'N' else 'south'
+        metrics['watershedArea'] = row[2]
+
+        # River lines
+        curs.execute('SELECT COUNT(*), Sum(Shape_Length), Min(TotDASqKm), Max(TotDASqKm) FROM riverlines')
+        row = curs.fetchone()
+        metrics['riverCount'] = row[0]
+        metrics['riverLength'] = row[1]
+        metrics['riverMinDrainageArea'] = row[2]
+        metrics['riverMaxDrainageArea'] = row[3]
+
+    # TODO: Include raster statistics in metrics
+
+    with open(output_file, 'w', encoding='utf8') as f:
+        json.dump(metrics, f, indent=2)
+
+
+def process_topography(input_dem: str, output_folder: str, processing_boundary) -> Tuple[str, str, str]:
+    """
+    Process the topography data for the specified watershed.
+    """
+
+    log = Logger('Topography')
+    log.info(f'Processing topography using DEM: {input_dem}')
+
+    topo_folder = os.path.join(output_folder, 'topography')
+    output_dem = os.path.join(topo_folder, 'dem.tif')
+    output_slope = os.path.join(topo_folder, 'slope.tif')
+    output_hillshade = os.path.join(topo_folder, 'dem_hillshade.tif')
+
+    raster_warp(input_dem, output_dem, 2193, processing_boundary, {"cutlineBlend": 1})
+    gdal_dem_geographic(output_dem, output_slope, 'slope')
+    gdal_dem_geographic(output_dem, output_hillshade, 'hillshade')
+
+    log.info(f'DEM produced at {output_dem}')
+    log.info(f'Slope produced at {output_slope}')
+    log.info(f'Hillshade produced at {output_hillshade}')
+    log.info('Topography processing complete')
+
+    return output_dem, output_slope, output_hillshade
+
+
+def main():
+    """ Main entry point for New Zealand RS Context"""
+    parser = argparse.ArgumentParser(description='Riverscapes Context Tool for Italy')
+    parser.add_argument('watershed_id', help='Watershed/HUC identifier', type=int)
+    parser.add_argument('hydro_gpkg', help='Path to GeoPackage containing national hydrography feature classes', type=str)
+    parser.add_argument('dem', help='Path to DEM raster.', type=str)
+    parser.add_argument('output', help='Path to the output folder', type=str)
+    parser.add_argument('--verbose', help='(optional) a little extra logging ', action='store_true', default=False)
+    parser.add_argument('--debug', help='(optional) more output about things like memory usage. There is a performance cost', action='store_true', default=False)
+    parser.add_argument('--meta', help='riverscapes project metadata as comma separated key=value pairs', type=str)
+
+    args = dotenv.parse_args_env(parser)
+
+    log = Logger('RS Context')
+    log.setup(logPath=os.path.join(args.output, 'RSContextIT.log'), verbose=args.verbose)
+    log.title(f'Riverscapes Context IT For Watershed: {args.watershed_id}')
+
+    log.info(f'Watershed ID: {args.watershed_id}')
+    log.info(f'Model Version: {__version__}')
+    log.info(f'EPSG: {cfg.OUTPUT_EPSG}')
+    log.info(f'Output folder: {args.output}')
+
+    meta = parse_metadata(args.meta)
+
+    try:
+        rs_context_it(args.watershed_id, args.hydro_gpkg, args.dem, args.output, meta)
+    except Exception as e:
+        log.error(e)
+        traceback.print_exc(file=sys.stdout)
+        sys.exit(1)
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
