3864787922b6024b72040c8aa2a854496f4829a1
Philip Bailey <philipbaileynar@users.noreply.github.com>
2025-05-30 23:31:44 +0000

    Renaming rscommons copy_feature_class() for shapefiles

--- packages/rscontext/rscontext/clip_vector.py
+++ packages/rscontext/rscontext/clip_vector.py
@@ -3,7 +3,7 @@ import argparse
 from rscommons import Logger, dotenv
 from rscommons.shapefile import get_geometry_unary_union
 from rscommons.shapefile import _rough_convert_metres_to_shapefile_units
-from rscommons.shapefile import copy_feature_class
+from rscommons.shapefile import copy_feature_class_shapefile
 from rscommons.shapefile import delete_shapefile
 
 
@@ -18,10 +18,10 @@ def clip_vector_layer(boundary, vector, out_path, output_epsg, buffer_meters, cl
     buffered = huc_boundary.buffer(buff_dist)
     if clip:
         log.info('Clipping {} feature class to {}m buffer around HUC boundary.'.format(os.path.basename(out_path), buffer_meters))
-        copy_feature_class(vector, output_epsg, out_path, clip_shape=buffered)
+        copy_feature_class_shapefile(vector, output_epsg, out_path, clip_shape=buffered)
     else:
         log.info('Selecting features from {} feature class that intersect HUC boundary.'.format(os.path.basename(out_path)))
-        copy_feature_class(vector, output_epsg, out_path, intersect_shape=buffered)
+        copy_feature_class_shapefile(vector, output_epsg, out_path, intersect_shape=buffered)
     log.info(f'{os.path.basename(vector)} clip complete.')
 
 
