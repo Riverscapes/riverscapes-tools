cd1c2bd267df9c5790f8c867d0d9086306de7186
Jordan Gilbert <jtgilbert89@gmail.com>
2025-06-30 18:08:47 +0000

    updated measurement calcs for flowlines that cross dgos

--- packages/rme/rme/utils/measurements.py
+++ packages/rme/rme/utils/measurements.py
@@ -29,7 +29,7 @@ def get_segment_measurements(geom_line: ogr.Geometry, src_raster: rasterio.Datas
     endpoints = get_endpoints(geom_clipped)
     elevations = [None, None]
     straight_length = None
-    if len(endpoints) == 2:
+    if len(endpoints) >= 2:
         elevations = []
         for pnt in endpoints:
             point = Point(pnt)
@@ -39,12 +39,15 @@ def get_segment_measurements(geom_line: ogr.Geometry, src_raster: rasterio.Datas
             mask_raster = np.ma.masked_values(raw_raster, src_raster.nodata)
             value = float(mask_raster.min())  # BRAT uses mean here
             elevations.append(value)
-        elevations.sort()
-        straight = LineString([endpoints[0], endpoints[1]])
-        straight_ogr = VectorBase.shapely2ogr(straight)
-        straight_ogr.Transform(transform)
-        straight_length = straight_ogr.Length()
+    combined = zip(elevations, endpoints)
+    sorted_combined = sorted(combined)
+    sorted_elevs, sorted_epts = zip(*sorted_combined)
+    # elevations.sort()
+    straight = LineString([sorted_epts[0], sorted_epts[-1]])
+    straight_ogr = VectorBase.shapely2ogr(straight)
+    straight_ogr.Transform(transform)
+    straight_length = straight_ogr.Length()
     geom_clipped.Transform(transform)
     stream_length = geom_clipped.Length()
 
-    return stream_length, straight_length, elevations[0], elevations[1]
+    return stream_length, straight_length, sorted_elevs[0], sorted_elevs[-1]
