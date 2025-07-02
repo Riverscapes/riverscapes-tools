b3ca8765d462f447eee00c914dca11617004469f
Jagmeet Dhillon <jagmeetdhillon@Jagmeets-MacBook-Air.local>
2025-07-02 12:54:30 -0700

    Search

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -46,6 +46,39 @@ const config: Config = {
   themeConfig: {
     image: 'img/logo.png', // Social sharing image
 
+    algolia: {
+      // The application ID provided by Algolia
+      appId: "G4FMJDKSEN",
+
+      // Public API key: it is safe to commit it
+      apiKey: "e50194f7329eb014a3266e3fcf837bf9",
+
+      indexName: "Riverscapes Tools",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: "external\\.com|domain\\.com",
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: "/docs/", // or as RegExp: /\/docs\//
+      //   to: "/",
+      // },
+
+      // Optional: Algolia search parameters
+      // searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      // searchPagePath: "search",
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      // insights: false,
+
+      //... other Algolia params
+    },
+
     navbar: {
       title: 'Riverscapes Tools', // Navbar title
       logo: {
