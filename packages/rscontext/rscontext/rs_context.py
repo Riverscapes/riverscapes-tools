32930bd36d45b55195becbc2a684fc4da15cb2b3
Jordan Gilbert <jtgilbert89@gmail.com>
2025-07-08 04:30:53 +0000

    rm commented nwi lines

--- packages/rscontext/rscontext/rs_context.py
+++ packages/rscontext/rscontext/rs_context.py
@@ -334,15 +334,7 @@ def rs_context(huc: str, landfire_dir: str, ownership: str, fair_market: str, ec
     nwi_gpkg = os.path.join(output_folder, LayerTypes['NATIONAL_WETLANDS'].rel_path)
     log.info(f'Processing National Wetland Inventory. Output: {nwi_gpkg}')
     national_wetlands_inventory(huc, download_folder, nhd['WBDHU10'], nwi_gpkg, cfg.OUTPUT_EPSG)
-    # del_items = []
-    # for layer in LayerTypes['NATIONAL_WETLANDS'].sub_layers:
-    #     layer_path = os.path.join(nwi_gpkg, LayerTypes['NATIONAL_WETLANDS'].sub_layers[layer].rel_path)
-    #     if not os.path.exists(layer_path):
-    #         del_items.append(layer)
-    # log.info(f'Deleting {del_items} from Layer Types')  # for debug, remove
-    # for layer in del_items:
-    #     del (LayerTypes['NATIONAL_WETLANDS'].sub_layers[layer])
-    # if len(LayerTypes['NATIONAL_WETLANDS'].sub_layers) > 0:
+
     project.add_project_geopackage(datasets, LayerTypes['NATIONAL_WETLANDS'])
 
     ################################################################################################################################################
