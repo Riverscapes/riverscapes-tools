3bb599ea581bb1c4e56db49f98277c269c6c5541
Matt Reimer <matt@northarrowresearch.com>
2025-10-28 16:17:13 -0700

    finish the postbuild steps

--- .devcontainer/postBuild.sh
+++ .devcontainer/postBuild.sh
@@ -3,13 +3,6 @@
 # fix to use git internally thanks to error: "detected dubious ownership in repository"
 git config --global --add safe.directory /workspaces/riverscapes-tools
 
-# Link up rscommons so we can find it everywhere
-pip install -e ./lib/commons
-
-# Iterate over the tools and install them. This should catch anything the requirements.txt file missed
-# and it al
-for tooldir in packages/* ; do 
-  # Install our packages as being editable
-  pip install -e $tooldir
-done
-
+uv sync
+# We need to install whatever of GDAL matches the system version
+uv pip install gdal==$(gdal-config --version)
