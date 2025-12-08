a31809ebfb8adf2c7c0dd56ce4a23e3009633255
Joe Wheaton <joe.m.wheaton@gmail.com>
2025-12-07 17:18:25 -0700

    Update site title and tagline in config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic'
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Riverscapes Tools', // Site title displayed in the browser tab
-  tagline: 'Homepage for riverscapes tools documentation', // Short description shown in meta tags
+  title: 'Riverscapes Network Models', // Site title displayed in the browser tab
+  tagline: 'Homepage for Riverscapes Network Models documentation', // Short description shown in meta tags
   favicon: '/img/Consortium.svg', // Path to site favicon
 
   headTags: [
