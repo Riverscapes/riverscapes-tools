007537880d2a52a8fbb829823683fd83426a2cd2
Lorin <lorin@northarroresearch.com>
2026-03-19 13:35:56 -0700

    correct name of module

--- scripts/automation/FargateRSContext.sh
+++ scripts/automation/FargateRSContext.sh
@@ -65,7 +65,7 @@ echo "======================  Starting RSContext ======================="
 ##########################################################################################
 try() {
   cd /usr/local/src/riverscapes-tools/packages/rscontext  
-  python3 -m rscontext.rscontext $HUC \
+  python3 -m rscontext.rs_context $HUC \
     /efsshare/NationalDatasets/landfire/240/ \
     /efsshare/NationalDatasets/ownership/surface_management_agency.shp \
     /efsshare/NationalDatasets/ownership/FairMarketValue.tif \
